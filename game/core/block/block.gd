8a37ba9143dc706fb93e75c2fbbd3e1aeebd2f6c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-04 14:55:26 +0100

    get_model()

--- game/core/block/block.gd
+++ game/core/block/block.gd
@@ -5,3 +5,26 @@ extends NamedResource
 @export var texture: Texture2D
 @export var material: StandardMaterial3D
 @export var scene: PackedScene
+
+
+
+func get_model()-> Node3D:
+	var model: Node3D
+	
+	if scene:
+		model= scene.instantiate()
+	else:
+		model= MeshInstance3D.new()
+		model.mesh= BoxMesh.new()
+
+		var model_material: StandardMaterial3D
+		if material:
+			model_material= material
+		else:
+			model_material= StandardMaterial3D.new()
+		
+		if texture:
+			model_material.albedo_texture= texture
+		model.mesh.surface_set_material(0, model_material)
+	
+	return model

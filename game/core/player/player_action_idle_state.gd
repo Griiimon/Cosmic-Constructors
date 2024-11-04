fc22036d1fdf8984d62274c9a12cc5c8cdbc2408
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-04 20:07:33 +0100

    rename

--- game/core/player/player_action_idle_state.gd
+++ game/core/player/player_action_idle_state.gd
@@ -1,4 +1,4 @@
-class_name PlayerActionStateIdle
+class_name PlayerActionIdleState
 extends PlayerStateMachineState
 
 
@@ -10,9 +10,9 @@ func on_physics_process(_delta: float):
 			var grid: BlockGrid= shapecast.get_collider(0)
 			assert(grid)
 			var collision_pos: Vector3= shapecast.get_collision_point(0)
-			collision_pos-= shapecast.global_basis.z * 0.05
+			collision_pos-= shapecast.global_basis.z * 0.01
 			
 			var grid_block: GridBlock= grid.get_block_from_global_pos(collision_pos)
-			
-			if grid_block.block_definition.can_interact():
-				grid_block.block_instance.interact(grid, grid_block, player)
+			if grid_block:
+				if grid_block.block_definition.can_interact():
+					grid_block.block_instance.interact(grid, grid_block, player)

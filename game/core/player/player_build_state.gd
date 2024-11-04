22b48078be0aba988097c4f97197e5bd4e5492c0
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-04 14:55:34 +0100

    Create player_build_state.gd

---
+++ game/core/player/player_build_state.gd
@@ -0,0 +1,43 @@
+class_name PlayerBuildState
+extends PlayerStateMachineState
+
+@export var build_range: float= 4.0
+@export var current_block: Block
+
+var ghost: Node3D
+
+
+func on_enter():
+	player.build_raycast.enabled= true
+	player.build_raycast.target_position.z= -build_range
+	
+	init_ghost()
+
+
+func on_exit():
+	remove_child(ghost)
+	ghost.queue_free()
+
+
+func init_ghost():
+	ghost= current_block.get_model()
+	add_child(ghost)
+	ghost.hide()
+
+
+func on_physics_process(_delta: float):
+	var raycast: RayCast3D= player.build_raycast
+	if raycast.is_colliding():
+		var collision_pos: Vector3= raycast.get_collision_point()
+		collision_pos-= raycast.global_basis.z * 0.05
+		
+		var grid: BlockGrid= raycast.get_collider()
+		assert(grid != null)
+		
+		var local_block_pos: Vector3i= grid.get_local_grid_pos(collision_pos)
+		var global_block_pos: Vector3= grid.get_global_block_pos(local_block_pos)
+		ghost.global_position= global_block_pos
+		ghost.show()
+	else:
+		ghost.hide()
+		

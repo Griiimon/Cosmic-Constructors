876bdf64c4497a638559dc527bbc8bfe00721f58
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-04 19:02:35 +0100

    Update thruster_block_instance.gd

--- game/data/blocks/thruster/thruster_block_instance.gd
+++ game/data/blocks/thruster/thruster_block_instance.gd
@@ -5,3 +5,12 @@ extends BlockInstance
 
 func _process(delta: float) -> void:
 	particles.emitting= active
+
+
+func physics_tick(grid: BlockGrid, grid_block: GridBlock, delta: float):
+	assert(grid)
+	var thruster_block: ThrusterBlock= grid_block.block_definition
+	assert(thruster_block)
+	if not thruster_block.force_offset:
+		grid.apply_central_force(-grid_block.get_global_basis(grid).z * thruster_block.thrust * delta)
+	

59d2f1905b6d2d96b5422d516642081c4ba4f99b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-04 15:20:26 +0100

    mouse controls

--- game/core/player/player_eva_state.gd
+++ game/core/player/player_eva_state.gd
@@ -12,6 +12,9 @@ extends PlayerStateMachineState
 
 @export var dampeners_active: bool= true 
 
+var yaw_input: float
+var pitch_input: float
+var roll_input: float
 
 
 func on_enter():
@@ -20,18 +23,22 @@ func on_enter():
 
 
 func on_physics_process(delta: float):
-	var yaw_input= Input.get_axis("yaw_right", "yaw_left")
+	#var yaw_input= Input.get_axis("yaw_right", "yaw_left")
 	var roll_input= Input.get_axis("roll_right", "roll_left")
-	var pitch_input= Input.get_axis("pitch_down", "pitch_up")
-	
-	#var basis= player.transform.basis
-	#basis= basis.rotated(basis.z, roll_input * roll_factor * delta)
-	#basis= basis.rotated(basis.x, pitch_input * pitch_factor * delta)
-	#basis= basis.rotated(basis.y, yaw_input * yaw_factor * delta)
-#
-	#player.transform.basis= basis
-	player.apply_torque(player.to_global(Vector3(pitch_input * pitch_factor, yaw_input * yaw_factor, roll_input * roll_factor)) * delta)
+	#var pitch_input= Input.get_axis("pitch_down", "pitch_up")
+
+	var input_torque: Vector3= player.to_global(Vector3(pitch_input * pitch_factor, yaw_input * yaw_factor, roll_input * roll_factor))
+	pitch_input= 0
+	yaw_input= 0
+
+	player.apply_torque(input_torque * delta)
 
 	var move_input= Input.get_axis("move_back", "move_forward")
 	
 	player.apply_central_force(move_input * -player.global_basis.z * acceleration)
+
+
+func on_input(event: InputEvent):
+	if event is InputEventMouseMotion:
+		yaw_input= -event.relative.x
+		pitch_input= -event.relative.y

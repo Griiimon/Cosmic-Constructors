8042eb5ac29eaaf1d623ae235c5e7fbec909d7b1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-05 00:16:51 +0100

    fix enter seat

--- game/core/player/player_seated_state.gd
+++ game/core/player/player_seated_state.gd
@@ -3,12 +3,21 @@ extends PlayerStateMachineState
 
 
 var seat: SeatInstance
-var tween: Tween
+#var tween: Tween
 
 
 func on_enter():
+	player.freeze_mode= RigidBody3D.FREEZE_MODE_STATIC
 	player.freeze= true
+	player.collision_shape.disabled= true
+	await get_tree().physics_frame
 
-	if tween: tween.kill()
-	tween= create_tween()
-	tween.tween_property(player, "global_basis", seat.global_basis, 0.25)
+	#if tween: tween.kill()
+	#tween= create_tween()
+	#tween.tween_property(player, "global_basis", seat.global_basis, 0.25)
+	
+	player.reparent(seat, false)
+	player.transform= Transform3D.IDENTITY
+
+#func on_process(delta: float):
+	

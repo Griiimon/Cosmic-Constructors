f1e105a432c27b7f86209058f99dea21341ab9b1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-04 20:07:26 +0100

    implement sitting

--- game/core/player/player.gd
+++ game/core/player/player.gd
@@ -4,7 +4,13 @@ extends Entity
 @onready var build_raycast: RayCast3D = %"Build Raycast"
 @onready var interact_shapecast: ShapeCast3D = %"Interact Shapecast"
 
+@onready var movement_state_machine: PlayerMovementStateMachine = $"Movement State Machine"
+@onready var action_state_machine: PlayerActionStateMachine = $"Action State Machine"
 
 
 func _ready() -> void:
 	Input.mouse_mode= Input.MOUSE_MODE_CAPTURED
+
+
+func sit(seat: SeatInstance):
+	movement_state_machine.sit(seat)
--- game/core/player/player_movement_state_machine.gd
+++ game/core/player/player_movement_state_machine.gd
@@ -1,2 +1,12 @@
 class_name PlayerMovementStateMachine
 extends FiniteStateMachine
+
+
+@onready var eva_state: PlayerEvaState = $EVA
+@onready var seated_state: PlayerSeatedState = $Seated
+
+
+
+func sit(seat: SeatInstance):
+	seated_state.seat= seat
+	change_state(seated_state)

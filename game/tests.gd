7b13cac90261133fbb158839b9b9831fa12052f1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-04 20:08:27 +0100

    Update tests.gd

--- game/tests.gd
+++ game/tests.gd
@@ -6,5 +6,8 @@ func _ready() -> void:
 	var default_block= load("res://game/data/blocks/light structure/light_structure_block.tres")
 	var grid:= BlockGrid.new()
 	grid.position.z= -2
-	grid.add_block(default_block, Vector3i.ZERO)
+	for x in 4:
+		for z in 4:
+			grid.add_block(default_block, Vector3i(x, 0, z))
+
 	add_child(grid)

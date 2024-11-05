c68b2fa5e366d6ffcbe9de23c46939e24b52749d
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-05 09:14:13 +0100

    move logic up a class

--- game/core/player/player_grid_move_state.gd
+++ game/core/player/player_grid_move_state.gd
@@ -1,25 +1,16 @@
 class_name PlayerGridMoveState
 extends PlayerMovementStateMachineState
 
-signal jetpack_enabled
 
 var grid: BlockGrid
 
 
 
 func on_enter():
-	player.collision_shape.disabled= true
-	await get_tree().physics_frame
-	
-	player.global_transform= Utils.align_with_y(player.global_transform, get_floor_normal())
+	super()
 	player.reparent(grid)
 
 
 func on_exit():
-	player.collision_shape.disabled= false
-
-
-func on_physics_process(_delta: float):
-	if Input.is_action_just_pressed("jetpack"):
-		jetpack_enabled.emit()
-		return
+	super()
+	player.reparent(get_tree().current_scene)

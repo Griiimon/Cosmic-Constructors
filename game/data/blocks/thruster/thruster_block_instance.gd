116497487e0db293141a20f537ca059033f1e29f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-06 13:42:11 +0100

    Update thruster_block_instance.gd

--- game/data/blocks/thruster/thruster_block_instance.gd
+++ game/data/blocks/thruster/thruster_block_instance.gd
@@ -30,17 +30,16 @@ func physics_tick(grid: BlockGrid, grid_block: GridBlock, delta: float):
 	if not active:
 		if not grid.requested_movement.is_zero_approx():
 			var local_thrust: Vector3= get_local_thrust_direction(grid_block)
-			#DebugHud.send("LT " + name.right(8), local_thrust)
 			if local_thrust.dot(grid.requested_movement) > 0.1:
 				tmp_active= true
 				active= true
 
 	if not active: return
-	assert(grid)
 	var thruster_block: ThrusterBlock= grid_block.block_definition
-	assert(thruster_block)
+
 	if not thruster_block.force_offset:
-		grid.apply_central_force(-grid_block.get_global_basis(grid).z * thruster_block.thrust * delta)
+		#grid.apply_central_force(-grid_block.get_global_basis(grid).z * thruster_block.thrust * delta)
+		grid.apply_central_force(-global_basis.z * thruster_block.thrust * delta)
 
 	if tmp_active:
 		active= false

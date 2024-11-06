ddda7f5cfa922512973d2626b7d61d4520811445
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-06 18:45:15 +0100

    Update game.gd

--- game/game.gd
+++ game/game.gd
@@ -2,3 +2,15 @@ class_name Game
 extends Node3D
 
 @onready var player: Player = $Player
+
+var grids: Node
+
+
+func _init():
+	Global.game= self
+
+
+func _ready() -> void:
+	grids= Node.new()
+	grids.name= "Grids"
+	add_child(grids)

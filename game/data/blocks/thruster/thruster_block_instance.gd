787c7ed49aca1d5ec9f8cd8e627030687f187771
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-06 04:21:42 +0100

    particle cooldown

--- game/data/blocks/thruster/thruster_block_instance.gd
+++ game/data/blocks/thruster/thruster_block_instance.gd
@@ -2,15 +2,28 @@ extends BlockInstance
 
 @onready var particles: CPUParticles3D = $CPUParticles3D
 
+var particle_stop_delay: Timer
 
 
 func _ready() -> void:
+	particle_stop_delay= Timer.new()
+	particle_stop_delay.one_shot= true
+	particle_stop_delay.wait_time= 0.2
+	particle_stop_delay.timeout.connect(func(): particles.emitting= false)
+	add_child(particle_stop_delay)
+
 	active= false
 
 
-func _process(delta: float) -> void:
-	particles.emitting= active
-
+func on_set_active():
+	if not is_inside_tree(): return
+	
+	if active:
+		particles.emitting= true
+		particle_stop_delay.stop()
+	else:
+		particle_stop_delay.start()
+	
 
 func physics_tick(grid: BlockGrid, grid_block: GridBlock, delta: float):
 	var tmp_active:= false

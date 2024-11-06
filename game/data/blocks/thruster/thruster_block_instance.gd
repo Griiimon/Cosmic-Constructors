ddfc02c7fa65e9265b0fabf56c41db3a241977c5
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-06 06:14:15 +0100

    Update thruster_block_instance.gd

--- game/data/blocks/thruster/thruster_block_instance.gd
+++ game/data/blocks/thruster/thruster_block_instance.gd
@@ -40,7 +40,7 @@ func physics_tick(grid: BlockGrid, grid_block: GridBlock, delta: float):
 	var thruster_block: ThrusterBlock= grid_block.block_definition
 	assert(thruster_block)
 	if not thruster_block.force_offset:
-		grid.apply_central_force(grid_block.get_global_basis(grid).z * thruster_block.thrust * delta)
+		grid.apply_central_force(-grid_block.get_global_basis(grid).z * thruster_block.thrust * delta)
 
 	if tmp_active:
 		active= false

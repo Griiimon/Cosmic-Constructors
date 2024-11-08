9accc820398859d9cdcad108f49896c70b379c5c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-08 03:05:11 +0100

    use shapecast

--- game/data/blocks/drill/drill.gd
+++ game/data/blocks/drill/drill.gd
@@ -3,8 +3,7 @@ extends Node3D
 @export var rotation_speed: float= 5.0
 
 @onready var cooldown: Timer = $Cooldown
-@onready var drill_area: Area3D = $"Drill Area"
-@onready var drill_area_collision_shape: CollisionShape3D = $"Drill Area/CollisionShape3D"
+@onready var terrain_shapecast: ShapeCast3D = $"Terrain ShapeCast"
 @onready var drill_head: MeshInstance3D = $"Drill Head"
 
 
@@ -15,12 +14,12 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if cooldown.is_stopped():
-		if drill_area.has_overlapping_bodies():
-			var terrain: VoxelLodTerrain= drill_area.get_overlapping_bodies()[0]
+		if terrain_shapecast.is_colliding():
+			var terrain: VoxelLodTerrain= terrain_shapecast.get_collider(0)
 			var tool: VoxelToolLodTerrain= terrain.get_voxel_tool()
 			tool.channel= VoxelBuffer.CHANNEL_SDF
 			tool.mode= VoxelTool.MODE_REMOVE
-			var radius: float= (drill_area_collision_shape.shape as SphereShape3D).radius
-			tool.do_sphere(terrain.to_local(to_global(drill_area.position)), radius)
+			var radius: float= (terrain_shapecast.shape as SphereShape3D).radius
+			tool.do_sphere(terrain.to_local(terrain_shapecast.get_collision_point(0)), radius)
 	
 		cooldown.start()

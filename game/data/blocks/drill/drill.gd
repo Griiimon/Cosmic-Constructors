92dcc89837198fd1ab9ad58619e70987a47e98be
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-08 02:56:42 +0100

    use drill area

--- game/data/blocks/drill/drill.gd
+++ game/data/blocks/drill/drill.gd
@@ -3,7 +3,8 @@ extends Node3D
 @export var rotation_speed: float= 5.0
 
 @onready var cooldown: Timer = $Cooldown
-@onready var terrain_raycast: RayCast3D = $"Terrain RayCast"
+@onready var drill_area: Area3D = $"Drill Area"
+@onready var drill_area_collision_shape: CollisionShape3D = $"Drill Area/CollisionShape3D"
 @onready var drill_head: MeshInstance3D = $"Drill Head"
 
 
@@ -14,11 +15,12 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	if cooldown.is_stopped():
-		if terrain_raycast.is_colliding():
-			var terrain: VoxelLodTerrain= terrain_raycast.get_collider()
+		if drill_area.has_overlapping_bodies():
+			var terrain: VoxelLodTerrain= drill_area.get_overlapping_bodies()[0]
 			var tool: VoxelToolLodTerrain= terrain.get_voxel_tool()
 			tool.channel= VoxelBuffer.CHANNEL_SDF
 			tool.mode= VoxelTool.MODE_REMOVE
-			tool.do_sphere(terrain.to_local(to_global(terrain_raycast.target_position)), 2)
+			var radius: float= (drill_area_collision_shape.shape as SphereShape3D).radius
+			tool.do_sphere(terrain.to_local(to_global(drill_area.position)), radius)
 	
 		cooldown.start()

3183b4df2856e3a5a7690bf532dcd1c3095ca022
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-09 02:22:26 +0100

    asteroid texturing

---
+++ game/core/asteroid/asteroid.gd
@@ -0,0 +1,18 @@
+class_name Asteroid
+extends Node3D
+
+@onready var terrain: VoxelLodTerrain = $VoxelLodTerrain
+
+
+
+func _ready() -> void:
+	await get_tree().create_timer(1).timeout
+	var voxel_tool: VoxelToolLodTerrain= terrain.get_voxel_tool()
+
+	voxel_tool.mode = VoxelTool.MODE_TEXTURE_PAINT
+	voxel_tool.channel = VoxelBuffer.CHANNEL_SDF
+	voxel_tool.texture_index= 1
+	for i in 15:
+		var pos:= Vector3(randf_range(-50, 50), randf_range(-50, 50), randf_range(-50, 50))
+		voxel_tool.do_sphere(pos, randf_range(3, 7))
+	

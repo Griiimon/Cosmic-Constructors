26df87e2949ffbf70e44bcd37ffba37648f8552b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-09 21:05:58 +0100

    Create block_prop_bool.gd

---
+++ game/core/block/properties/block_prop_bool.gd
@@ -0,0 +1,29 @@
+class_name BlockPropBool
+extends BlockProperty
+
+var b: bool= false
+
+
+
+func _init(_b: bool= false, _callback= null):
+	super(_callback)
+	b= _b
+
+
+func toggle():
+	b= not b
+	super()
+
+
+func is_true()-> bool:
+	return b
+
+
+func set_true():
+	if not b:
+		toggle()
+
+
+func set_false():
+	if b:
+		toggle()

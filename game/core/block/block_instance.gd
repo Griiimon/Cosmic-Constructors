32ec0b0884083c48406904e2c68e5388dde8a66c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-09 21:05:44 +0100

    Update block_instance.gd

--- game/core/block/block_instance.gd
+++ game/core/block/block_instance.gd
@@ -2,9 +2,6 @@ class_name BlockInstance
 extends Node3D
 
 
-var active: bool= true: set= set_active
-
-
 
 func on_placed(grid: BlockGrid, grid_block: GridBlock):
 	pass
@@ -16,13 +13,3 @@ func physics_tick(grid: BlockGrid, grid_block: GridBlock, delta: float):
 
 func on_destroy():
 	queue_free()
-
-
-func set_active(b: bool):
-	if active == b: return
-	active= b
-	on_set_active()
-
-
-func on_set_active():
-	pass

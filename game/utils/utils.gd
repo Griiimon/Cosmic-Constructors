e17b25d420b1c74a1fb90f8889ccfdf797939e24
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-10 05:46:34 +0100

    Update utils.gd

--- game/utils/utils.gd
+++ game/utils/utils.gd
@@ -2,6 +2,10 @@ class_name Utils
 
 
 
+static func is_even(n: int)-> bool:
+	return is_equal_approx(int(n / 2), n / 2.0)
+
+
 static func align_with_y(xform: Transform3D, new_y: Vector3)-> Transform3D:
 	xform.basis.y = new_y
 	xform.basis.x = -xform.basis.z.cross(new_y)

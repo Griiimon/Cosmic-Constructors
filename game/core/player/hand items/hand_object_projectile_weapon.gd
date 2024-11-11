dd01f49c6a6d3322382faf890f7004746271a270
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-11 14:53:07 +0100

    Create hand_object_projectile_weapon.gd

---
+++ game/core/player/hand items/hand_object_projectile_weapon.gd
@@ -0,0 +1,13 @@
+extends HandObject
+
+@export var muzzle: Node3D
+
+
+
+func use():
+	var weapon_definition: ProjectileWeapon= item_definition
+	var projectile: ProjectileObject= weapon_definition.projectile.scene.instantiate()
+	projectile.position= player.to_global(muzzle.global_position)
+	projectile.rotation= player.pivot.global_rotation
+	player.world.add_projectile(projectile)
+	projectile.velocity= -projectile.global_basis.z * weapon_definition.muzzle_velocity

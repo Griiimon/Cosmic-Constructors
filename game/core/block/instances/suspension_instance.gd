096dc4320c2dd96ba63feaab9c731246152968a6
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-11 21:18:12 +0100

    add suspension

---
+++ game/core/block/instances/suspension_instance.gd
@@ -0,0 +1,19 @@
+extends BlockInstance
+
+var wheel: Wheel
+
+
+
+func on_placed(grid: BlockGrid, grid_block: GridBlock):
+	wheel= load("res://game/core/car/wheel_3x_3.tscn").instantiate()
+	wheel.position= position + global_basis.x
+	grid.add_child(wheel)
+
+
+func physics_tick(grid: BlockGrid, grid_block: GridBlock, delta: float):
+	pass
+
+
+func on_destroy():
+	wheel.queue_free()
+	queue_free()

83f817d00891076781d6a31709227aaccdd7a2ca
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-11 05:15:45 +0100

    block definition lookup

--- game/global/game_data.gd
+++ game/global/game_data.gd
@@ -2,3 +2,17 @@ extends Node
 
 
 @export var block_library: BlockLibrary
+
+
+var block_definition_lookup: Dictionary
+
+
+
+func _ready() -> void:
+	for block in block_library.blocks:
+		block_definition_lookup[block.get_display_name()]= block
+
+
+func get_block_definition(block_name: String)-> Block:
+	assert(block_definition_lookup.has(block_name))
+	return block_definition_lookup[block_name]

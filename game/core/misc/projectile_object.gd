8910b78711a6024a41c6eaca237bd04d2afa4fec
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-11 17:10:11 +0100

    add collision detection

--- game/core/misc/projectile_object.gd
+++ game/core/misc/projectile_object.gd
@@ -1,10 +1,36 @@
 class_name ProjectileObject
 extends Node3D
 
+@export var shape: Shape3D
+
 var projectile_definition: Projectile
 var velocity: Vector3
+var shapecast: ShapeCast3D
 
+
+
+func _ready() -> void:
+	shapecast= ShapeCast3D.new()
+	shapecast.shape= shape
+	shapecast.collision_mask= Global.PLAYER_COLLISION_LAYER + Global.GRID_COLLISION_LAYER + Global.TERRAIN_COLLISION_LAYER
+	shapecast.top_level= true
+	add_child(shapecast)
 
 
 func _physics_process(delta: float) -> void:
+	var old_pos: Vector3= global_position
 	global_position+= velocity * delta
+	
+	shapecast.position= old_pos
+	shapecast.target_position= shapecast.to_local(global_position)
+	shapecast.force_shapecast_update()
+	if shapecast.is_colliding():
+		var collider: Node3D= shapecast.get_collider(0)
+		if collider.is_in_group(DamageComponent.GROUP_NAME):
+			var damage_component: DamageComponent= DamageComponent.get_component(collider)
+
+			var dmg: Damage= Damage.create_instance(projectile_definition.damage, velocity.normalized(), Damage.SourceType.PROJECTILE)
+			dmg.shape_index= shapecast.get_collider_shape(0)
+
+			damage_component.take_damage(dmg)
+		queue_free()

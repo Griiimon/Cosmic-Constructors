66c7fc2c4305039b6582de6f4855aefbe1567a80
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-11 17:10:23 +0100

    Update hand_object_projectile_weapon.gd

--- game/core/player/hand items/hand_object_projectile_weapon.gd
+++ game/core/player/hand items/hand_object_projectile_weapon.gd
@@ -7,6 +7,7 @@ extends HandObject
 func use():
 	var weapon_definition: ProjectileWeapon= item_definition
 	var projectile: ProjectileObject= weapon_definition.projectile.scene.instantiate()
+	projectile.projectile_definition= weapon_definition.projectile
 	projectile.position= player.to_global(muzzle.global_position)
 	projectile.rotation= player.pivot.global_rotation
 	player.world.add_projectile(projectile)

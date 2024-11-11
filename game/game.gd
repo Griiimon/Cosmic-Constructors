c4eef5da07539224c2f07538e0bff80d0392c3cc
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-11 21:50:29 +0100

    custom gravity

--- game/game.gd
+++ game/game.gd
@@ -1,6 +1,8 @@
 class_name Game
 extends Node3D
 
+@export var gravity: float
+
 @onready var player: Player = $Player
 @onready var world: World = $World
 
@@ -8,3 +10,7 @@ extends Node3D
 
 func _init():
 	Global.game= self
+
+
+func _ready() -> void:
+	PhysicsServer3D.area_set_param(get_world_3d().space, PhysicsServer3D.AREA_PARAM_GRAVITY, gravity)

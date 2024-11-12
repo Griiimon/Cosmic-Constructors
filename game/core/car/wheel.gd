8d21022e6a8059e68d4bbe83b8f2aebcff8498bb
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-12 07:05:45 +0100

    first cleanup iteration

--- game/core/car/wheel.gd
+++ game/core/car/wheel.gd
@@ -3,70 +3,52 @@ extends Node3D
 
 @export var shape : Shape3D
 @export_flags_3d_physics var mask : int = 1
-@export var castTo : Vector3 = Vector3(0,-1,0)
+@export var cast_to : Vector3 = Vector3(0,-3,0)
 
-@export var springMaxForce : float = 300.0
-@export var springForce : float = 180.0
-@export var stifness : float = 0.85
+@export var spring_max_force : float = 3000.0
+@export var spring_force : float = 1800.0
+@export var stiffness : float = 0.85
 @export var damping : float = 0.05
-@export var Xtraction : float = 1.0
-@export var Ztraction : float = 0.15
-@export var staticSlideThreshold : float = 0.005
-@export var massKG : float = 100.0
+@export var x_traction : float = 0.1
+@export var z_traction : float = 0.15
+@export var static_slide_threshold : float = 0.005
+@export var mass_kg : float = 100.0
 
 
-var instantLinearVelocity : Vector3
+var instant_linear_velocity : Vector3
 
 
-@onready var parentBody : BlockGrid = get_parent()
+@onready var parent_body : BlockGrid = get_parent()
 
-@onready var previousDistance : float = abs(castTo.y)
-var previousHit : ShapeCastResult = ShapeCastResult.new()
-var collisionPoint : Vector3 = castTo
+@onready var previous_distance : float = abs(cast_to.y)
+var previous_hit : Shapecast_result = Shapecast_result.new()
+var collision_point : Vector3 = cast_to
 var grounded : bool = false
 
 
-
-
-
-
 # shape cast result storage class
-class ShapeCastResult:
+class Shapecast_result:
 	var hit_distance : float
 	var hit_position : Vector3
 	var hit_normal : Vector3
 	var hit_point_velocity : Vector3
 	var hit_body : PhysicsBody3D
 
+
+
 # function to do sphere casting
 func shape_cast(origin: Vector3, offset: Vector3):
 	var space: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-	#var params: = PhysicsShapeQueryParameters3D.new()
-	#params.collision_mask = mask
-	#params.set_shape(shape)
-	#params.transform = transform
-	#params.transform.origin = origin
-	## exclude parent body!
-	#params.exclude = [parentBody]
-	#params.motion= offset
-	#
-	## cast motion to get max motion possible with this cast
-	#var castResult = space.cast_motion(params)
 
 	var query:= PhysicsRayQueryParameters3D.create(origin, origin + offset, mask)
-	var castResult= space.intersect_ray(query)
+	var cast_result= space.intersect_ray(query)
 
-	var result : ShapeCastResult = ShapeCastResult.new()
+	var result : Shapecast_result = Shapecast_result.new()
 	
-	result.hit_distance = origin.distance_to(castResult.position) if castResult else castTo.length()
-	result.hit_position = castResult.position if castResult else origin + offset
+	result.hit_distance = origin.distance_to(cast_result.position) if cast_result else cast_to.length()
+	result.hit_position = cast_result.position if cast_result else origin + offset
 	
-	# offset the params to the cast hit point and get rest info for more information
-	#params.transform.origin += offset * castResult[1]
-	#var collision = space.get_rest_info(params)
-	
-	#result.hit_normal = collision.get("normal", Vector3.ZERO)
-	result.hit_normal = castResult.normal if castResult else Vector3.ZERO
+	result.hit_normal = cast_result.normal if cast_result else Vector3.ZERO
 	result.hit_point_velocity = Vector3.ZERO
 	result.hit_body = null
 	
@@ -89,87 +71,87 @@ func shape_cast(origin: Vector3, offset: Vector3):
 
 # set forward friction (braking)
 func apply_brake(amount : float = 0.0) -> void:
-	Ztraction = max(0.0, amount)
+	z_traction = max(0.0, amount)
 
 
 # function for applying drive force to parent body (if grounded)
 func apply_force(force : Vector3) -> void:
 	if grounded:
-		parentBody.add_force(force, collisionPoint - parentBody.global_transform.origin)
+		parent_body.add_force(force, collision_point - parent_body.global_transform.origin)
 
 
 func _physics_process(delta) -> void:
 	# perform sphere cast
-	var castResult = shape_cast(global_transform.origin, castTo)
-	collisionPoint = castResult.hit_position
+	var cast_result = shape_cast(global_transform.origin, cast_to)
+	collision_point = cast_result.hit_position
 	#if GameState.debugMode:
 		#DrawLine3D.DrawCube(global_transform.origin,0.1,Color(255,0,255))
-		#DrawLine3D.DrawCube(global_transform.origin + castTo,0.1,Color(255,128,255))
+		#DrawLine3D.DrawCube(global_transform.origin + cast_to,0.1,Color(255,128,255))
 	# [1, 1] means no hit (from docs)
-	#if castResult.hit_distance != abs(castTo.y):
-	if not is_equal_approx(castResult.hit_distance, abs(castTo.y)):
+	#if cast_result.hit_distance != abs(cast_to.y):
+	if not is_equal_approx(cast_result.hit_distance, abs(cast_to.y)):
 		# if grounded, handle forces
 		grounded = true
-#		collisionPoint = castResult.hit_position
+#		collision_point = cast_result.hit_position
 		#if GameState.debugMode:
-			#DrawLine3D.DrawCube(castResult.hit_position,0.04,Color(0,255,255))
-			#DrawLine3D.DrawRay(castResult.hit_position,castResult.hit_normal,Color(255,255,255))
+			#DrawLine3D.DrawCube(cast_result.hit_position,0.04,Color(0,255,255))
+			#DrawLine3D.DrawRay(cast_result.hit_position,cast_result.hit_normal,Color(255,255,255))
 		
 		# obtain instantaneaous linear velocity
-		instantLinearVelocity = (collisionPoint - previousHit.hit_position) / delta
+		instant_linear_velocity = (collision_point - previous_hit.hit_position) / delta
 		
 		# apply spring force with damping force
-		var curDistance : float = castResult.hit_distance
-		var FSpring : float = stifness * (abs(castTo.y) - curDistance) 
-		var FDamp : float = damping * (previousDistance - curDistance) / delta
-		var suspensionForce : float = clamp((FSpring + FDamp) * springForce,0,springMaxForce)
-		var suspensionForceVec : Vector3 = castResult.hit_normal * suspensionForce
+		var current_distance : float = cast_result.hit_distance
+		var f_spring : float = stiffness * (abs(cast_to.y) - current_distance) 
+		var f_damp : float = damping * (previous_distance - current_distance) / delta
+		var suspension_force : float = clamp((f_spring + f_damp) * spring_force,0,spring_max_force)
+		var suspension_force_vec : Vector3 = cast_result.hit_normal * suspension_force
 		
 		# obtain axis velocity
-		#var localVelocity : Vector3 = global_transform.basis.xform_inv(instantLinearVelocity - castResult.hit_point_velocity) 
+		#var local_velocity : Vector3 = global_transform.basis.xform_inv(instant_linear_velocity - cast_result.hit_point_velocity) 
 		# TODO check if translated correctly
-		var localVelocity : Vector3 = (instantLinearVelocity - castResult.hit_point_velocity) * global_transform.basis
+		var local_velocity : Vector3 = (instant_linear_velocity - cast_result.hit_point_velocity) * global_transform.basis
 		
 		# axis deceleration forces based on this drive elements mass and current acceleration
-		var XAccel : float = (-localVelocity.x * Xtraction) / delta
-		var ZAccel : float = (-localVelocity.z * Ztraction) / delta
-		var XForce : Vector3 = global_transform.basis.x * XAccel * massKG
-		var ZForce : Vector3 = global_transform.basis.z * ZAccel * massKG
+		var x_accel : float = (-local_velocity.x * x_traction) / delta
+		var z_accel : float = (-local_velocity.z * z_traction) / delta
+		var x_force : Vector3 = global_transform.basis.x * x_accel * mass_kg
+		var z_force : Vector3 = global_transform.basis.z * z_accel * mass_kg
 		
 		# counter sliding by negating off axis suspension impulse at very low speed
-		var vLimit : float = instantLinearVelocity.length_squared() * delta
-		if vLimit < staticSlideThreshold:
-#			suspensionForceVec = Vector3.UP * suspensionForce
-			XForce.x -= suspensionForceVec.x * parentBody.global_transform.basis.y.dot(Vector3.UP)
-			ZForce.z -= suspensionForceVec.z * parentBody.global_transform.basis.y.dot(Vector3.UP)
+		var v_limit : float = instant_linear_velocity.length_squared() * delta
+		if v_limit < static_slide_threshold:
+#			suspension_force_vec = Vector3.UP * suspension_force
+			x_force.x -= suspension_force_vec.x * parent_body.global_transform.basis.y.dot(Vector3.UP)
+			z_force.z -= suspension_force_vec.z * parent_body.global_transform.basis.y.dot(Vector3.UP)
 		
 		# final impulse force vector to be applied
-		var finalForce = suspensionForceVec + XForce + ZForce
+		var final_force = suspension_force_vec + x_force + z_force
 		
 		# draw debug lines
 		#if GameState.debugMode:
-			#DrawLine3D.DrawRay(get_collision_point(),suspensionForceVec/GameState.debugRayScaleFac,Color(0,255,0))
-			#DrawLine3D.DrawRay(get_collision_point(),XForce/GameState.debugRayScaleFac,Color(255,0,0))
-			#DrawLine3D.DrawRay(get_collision_point(),ZForce/GameState.debugRayScaleFac,Color(0,0,255))
+			#DrawLine3D.DrawRay(get_collision_point(),suspension_force_vec/GameState.debugRayScaleFac,Color(0,255,0))
+			#DrawLine3D.DrawRay(get_collision_point(),x_force/GameState.debugRayScaleFac,Color(255,0,0))
+			#DrawLine3D.DrawRay(get_collision_point(),z_force/GameState.debugRayScaleFac,Color(0,0,255))
 			
 		# apply forces relative to parent body
-		DebugHud.send("Suspension force", finalForce)
-		parentBody.apply_force(finalForce, collisionPoint - parentBody.global_transform.origin)
+		DebugHud.send("Suspension force", final_force)
+		parent_body.apply_force(final_force, collision_point - parent_body.global_transform.origin)
 		
 		# apply forces to body affected by this drive element (action = reaction)
-		if castResult.hit_body && castResult.hit_body is RigidBody3D:
-			castResult.hit_body.apply_force(-finalForce, collisionPoint - castResult.hit_body.global_transform.origin)
+		if cast_result.hit_body && cast_result.hit_body is RigidBody3D:
+			cast_result.hit_body.apply_force(-final_force, collision_point - cast_result.hit_body.global_transform.origin)
 		
 		# set the previous values at the very end, after they have been used
-		previousDistance = curDistance
-		previousHit = castResult
+		previous_distance = current_distance
+		previous_hit = cast_result
 	else:
 		# not grounded, set prev values to fully extended suspension
 		grounded = false
-		previousHit = ShapeCastResult.new()
-		previousHit.hit_position = global_transform.origin + castTo
-		previousHit.hit_distance = abs(castTo.y)
-		previousDistance = previousHit.hit_distance
-		instantLinearVelocity = Vector3.ZERO
+		previous_hit = Shapecast_result.new()
+		previous_hit.hit_position = global_transform.origin + cast_to
+		previous_hit.hit_distance = abs(cast_to.y)
+		previous_distance = previous_hit.hit_distance
+		instant_linear_velocity = Vector3.ZERO
 
 	#DebugHud.send("Grounded", grounded)

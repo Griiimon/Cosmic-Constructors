eaa466ebcddefc033083b1a90248cbf3d68fc670
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-14 00:27:27 +0100

    re-project muzzle position

--- game/core/player/hand items/hand_object_projectile_weapon.gd
+++ game/core/player/hand items/hand_object_projectile_weapon.gd
@@ -8,7 +8,8 @@ func use():
 	var weapon_definition: ProjectileWeapon= item_definition
 	var projectile: ProjectileObject= weapon_definition.projectile.scene.instantiate()
 	projectile.projectile_definition= weapon_definition.projectile
-	projectile.position= player.to_global(muzzle.global_position)
+	projectile.position= player.first_person_camera.project_position(player.item_camera.unproject_position(muzzle.global_position), 1.0)
+	#projectile.position= player.to_global(muzzle.global_position)
 	projectile.rotation= player.pivot.global_rotation
 	player.world.add_projectile(projectile)
 	projectile.velocity= -projectile.global_basis.z * weapon_definition.muzzle_velocity

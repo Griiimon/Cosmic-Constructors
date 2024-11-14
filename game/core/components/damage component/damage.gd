a8305a554ba1fba4fc9dd7818b4ac9933865d61b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-14 09:32:18 +0100

    Update damage.gd

--- game/core/components/damage component/damage.gd
+++ game/core/components/damage component/damage.gd
@@ -7,14 +7,16 @@ enum SourceType { PROJECTILE, LASER, MELEE, BLOCKTOOL }
 @export var radius: float= 0.0
 @export var min_amount: int= 0
 
+var position: Vector3
 var source_type: SourceType
 var direction: Vector3
 var shape_index: int
 
 
 
-static func create_instance(orig_damage: Damage, _direction: Vector3, _source_type: SourceType)-> Damage:
+static func create_instance(orig_damage: Damage, _position: Vector3, _direction: Vector3, _source_type: SourceType)-> Damage:
 	var new_damage: Damage= orig_damage.duplicate()
+	new_damage.position= _position
 	new_damage.direction= _direction
 	new_damage.source_type= _source_type 
 	return new_damage

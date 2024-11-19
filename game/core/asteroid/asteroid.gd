d648459e700416bf13bd716ebfd440360d1dbdb1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-19 10:26:43 +0100

    Update asteroid.gd

--- game/core/asteroid/asteroid.gd
+++ game/core/asteroid/asteroid.gd
@@ -7,13 +7,15 @@ extends Node3D
 
 func _ready() -> void:
 	await get_tree().create_timer(3).timeout
+	if not is_instance_valid(terrain): return
+	
 	var voxel_tool: VoxelToolLodTerrain= terrain.get_voxel_tool()
 
 	voxel_tool.mode = VoxelTool.MODE_TEXTURE_PAINT
 	voxel_tool.channel = VoxelBuffer.CHANNEL_SDF
 	voxel_tool.texture_index= 1
 	var radius: float= 50.0
-	#radius*= terrain.scale.x
+	
 	radius/= terrain.scale.x
 	for i in 15:
 		var pos:= Vector3(randf_range(-radius, radius), randf_range(-radius, radius), randf_range(-radius, radius))

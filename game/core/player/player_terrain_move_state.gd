66ddb66c82ca71a44c857bd300ab2230acaea0c6
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-20 07:33:59 +0100

    align with gravity vec

--- game/core/player/player_terrain_move_state.gd
+++ game/core/player/player_terrain_move_state.gd
@@ -9,4 +9,7 @@ func initial_align():
 
 
 func continuous_align(delta: float):
-	player.global_transform= player.global_transform.interpolate_with(Utils.align_with_y(player.global_transform, get_floor_normal()), delta * 10)
+	if player.get_gravity().length():
+		player.global_transform= player.global_transform.interpolate_with(Utils.align_with_y(player.global_transform, -player.get_gravity().normalized()), delta * 10)
+	else:
+		player.global_transform= player.global_transform.interpolate_with(Utils.align_with_y(player.global_transform, get_floor_normal()), delta * 10)

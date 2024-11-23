8f7d11738db7a5423569970923324c05b34df4ed
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-23 22:10:10 +0100

    Update jetpack_thruster.gd

--- game/data/equipment/back/jetpack/jetpack_thruster.gd
+++ game/data/equipment/back/jetpack/jetpack_thruster.gd
@@ -1,9 +1,21 @@
 class_name JetpackThruster
 extends Node3D
 
+@export var thrust_vector: Vector3
+
 @onready var flame = $Flame
+@onready var activation_cooldown = $"Activation Cooldown"
 
 
 
-func set_thruster(b: bool):
+func set_active(b: bool):
 	flame.visible= b
+	#if b:
+		#activation_cooldown.start()
+	#else:
+		##activation_cooldown.stop()
+		#flame.visible= false
+
+
+func _on_activation_cooldown_timeout():
+		flame.visible= true

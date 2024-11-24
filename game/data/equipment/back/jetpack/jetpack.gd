cb45f489129cc5902401ec24cb3a0f9534e098f3
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-24 13:43:49 +0100

    deactivate thrusters when turned off

--- game/data/equipment/back/jetpack/jetpack.gd
+++ game/data/equipment/back/jetpack/jetpack.gd
@@ -5,11 +5,19 @@ extends PlayerEquipmentObject
 
 
 
+func init(player: Player):
+	player.movement_state_machine.eva_state.state_exited.connect(deactivate_thrusters)
+
+
 func tick(player: Player, delta: float):
 	if not player.movement_state_machine.eva_state.is_current_state(): return
 	var move_vec: Vector3= player.movement_state_machine.eva_state.move_vec
-	#if not move_vec: return
 	
 	for thruster: JetpackThruster in thrusters.get_children():
-		thruster.set_active(thruster.thrust_vector.dot(move_vec) > 0)
+		thruster.set_active(thruster.thrust_vector.dot(move_vec) > 0.9)
+
+
+func deactivate_thrusters():
+	for thruster: JetpackThruster in thrusters.get_children():
+		thruster.set_active(false)
 		

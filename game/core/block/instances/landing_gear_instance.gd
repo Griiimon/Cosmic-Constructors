7247cccdb5fabd35bf9e3513c036365d19c8f78d
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-25 08:09:34 +0100

    toggle on/off

--- game/core/block/instances/landing_gear_instance.gd
+++ game/core/block/instances/landing_gear_instance.gd
@@ -1,10 +1,27 @@
 class_name LandingGearInstance
-extends BlockInstance
+extends BlockInstanceOnOff
+
+@export var enabled_material: StandardMaterial3D
+@export var disabled_material: StandardMaterial3D
 
 @onready var area: Area3D = $Area3D
+@onready var light: MeshInstance3D = $Light
 
+
+
+func _ready() -> void:
+	default_interaction_property= active
+	active.set_true()
 
 
 func physics_tick(grid: BlockGrid, _grid_block: GridBlock, _delta: float):
-	if area.has_overlapping_bodies():
-		grid.freeze= true
+	if active.is_true():
+		if area.has_overlapping_bodies():
+			grid.freeze= true
+	else:
+		grid.freeze= false
+
+
+func on_set_active():
+	var material: StandardMaterial3D= enabled_material if active.is_true() else disabled_material
+	light.mesh.surface_set_material(0, material)

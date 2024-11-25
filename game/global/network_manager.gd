0b3fc706afb906acad046a09019c21976325ec48
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-25 10:21:18 +0100

    Update network_manager.gd

--- game/global/network_manager.gd
+++ game/global/network_manager.gd
@@ -1,39 +1,39 @@
 extends Node
 
+signal player_connected(id: int)
+signal player_disconnected(id: int)
+
 var single_player:= true
 var is_dedicated:= false
 
-const PUBLIC_SERVER_IP= "212.227.166.210"
+const DEFAULT_SERVER_IP= "127.0.0.1"
+
 var DEFAULT_PORT = 9999
 var enet_peer: ENetMultiplayerPeer
+var peer_id: int= -1
 
 
-
-func start_game():
+func run():
 	if single_player:
-		print("  Start Singleplayer")
+		print(" Start Singleplayer")
 		return
 	
 	enet_peer = ENetMultiplayerPeer.new()
+	
 	if is_dedicated:
-		print("  Hosting")
+		print(" Hosting")
 		do_host()
+	else:
+		join(DEFAULT_SERVER_IP, DEFAULT_PORT)
 
 
 func do_start():
-	get_tree().change_scene_to_file(get_room_file())
+	peer_id= multiplayer.get_unique_id()
 
-		
 
 func do_host():
 	var port: int= DEFAULT_PORT
 	
-	if not Globals.is_dedicated:
-		if %PortLineEdit.text.is_valid_int():
-			port= int(%PortLineEdit.text)
-	else:
-		Server.init_dedicated()
-
 	print("Creating server on port ", port, " ..")
 	var error= enet_peer.create_server(port)
 	if error != OK:
@@ -51,9 +51,22 @@ func join(address: String, port: int):
 	
 	var error= enet_peer.create_client(address, port)
 	if error != OK:
+		prints(" Joining failed:", error)
 		return
 	
 	multiplayer.multiplayer_peer = enet_peer
-	
+
 	multiplayer.connected_to_server.connect(do_start)
 	multiplayer.connection_failed.connect(connection_failed.bind("Connection failed"))
+
+
+func add_player(id: int):
+	player_connected.emit(id)
+
+
+func remove_player(id: int):
+	player_disconnected.emit(id)
+
+
+func connection_failed(s: String):
+	prints("Connection failed:", s)

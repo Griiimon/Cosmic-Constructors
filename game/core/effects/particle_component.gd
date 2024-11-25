6b43ee4ca50c975d7cb09236b1bdaa168ba088d0
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-25 07:47:47 +0100

    Update particle_component.gd

--- game/core/effects/particle_component.gd
+++ game/core/effects/particle_component.gd
@@ -3,11 +3,13 @@ extends CPUParticles3D
 
 @export var auto_start_emitting: bool= true
 @export var free_on_finished: bool= true
-
+@export var wait_for_parent: bool= false
 
 
 func _ready() -> void:
-	
+	if wait_for_parent:
+		await get_parent().ready
+		
 	if free_on_finished:
 		finished.connect(owner.queue_free)
 	if auto_start_emitting:

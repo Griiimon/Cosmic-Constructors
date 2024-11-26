e7a34d0c48a4c1facb962f4d506ba93738d04bed
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-26 17:52:01 +0100

    Update event_sync_state.gd

--- game/core/network/event_sync_state.gd
+++ game/core/network/event_sync_state.gd
@@ -1,3 +1,14 @@
 class_name EventSyncState
 
-const KEY_PLAYER_ANIMATION= "anim"
+enum Type { START_PLAYER_ANIMATION, RESET_PLAYER_ANIMATION, WEAR_EQUIPMENT, CLEAR_EQUIPMENT_SLOT }
+
+#var type: Type
+#var param: Variant
+
+
+static func process_event(type: Type, param: Variant, peer_id: int):
+	match type:
+		Type.START_PLAYER_ANIMATION:
+		Type.RESET_PLAYER_ANIMATION:
+		Type.WEAR_EQUIPMENT:
+		Type.CLEAR_EQUIPMENT_SLOT:

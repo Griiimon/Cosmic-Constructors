874d107f4458755dc71028a4c799320b9a84b3cd
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-26 08:05:24 +0100

    Update server_manager.gd

--- game/global/network managers/server_manager.gd
+++ game/global/network managers/server_manager.gd
@@ -9,9 +9,9 @@ func _ready() -> void:
 	set_physics_process(false)
 
 
-func do_host(port: int):
+func host(port: int, game_scene: PackedScene):
 	print("Creating server on port ", port, " ..")
-	var error= NetworkManager.Nenet_peer.create_server(port)
+	var error= NetworkManager.enet_peer.create_server(port)
 	if error != OK:
 		prints("Couldnt create server", error)
 		return
@@ -20,7 +20,8 @@ func do_host(port: int):
 	#multiplayer.peer_connected.connect(dummy)
 	multiplayer.peer_connected.connect(add_player)
 	multiplayer.peer_disconnected.connect(remove_player)
-	set_physics_process(true)
+
+	get_tree().change_scene_to_packed.call_deferred(game_scene)
 
 
 func _physics_process(delta: float) -> void:
@@ -30,6 +31,7 @@ func _physics_process(delta: float) -> void:
 
 func add_player(id: int):
 	player_connected.emit(id)
+	set_physics_process(true)
 
 
 func remove_player(id: int):

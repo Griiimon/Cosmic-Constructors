e3ae9f45fc3121b2549154cbe65129f6a4650640
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-26 16:03:13 +0100

    Update player_sync_state.gd

--- game/core/player/player_sync_state.gd
+++ game/core/player/player_sync_state.gd
@@ -1,5 +1,6 @@
 class_name PlayerSyncState
 
+const KEY_PEER_ID = "id"
 const KEY_POSITION = "pos"
 const KEY_ROTATION = "rot"
 const KEY_TIMESTAMP = "time"
@@ -17,6 +18,22 @@ static func build_sync_state(player: Player)-> Dictionary:
 static func parse_sync_state(player: BasePlayer, data: Dictionary):
 	player.global_position= data[KEY_POSITION]
 	player.global_rotation= data[KEY_ROTATION]
+
+
+static func add_peer_id(data: Dictionary, peer_id: int):
+	data[KEY_PEER_ID]= peer_id
+
+
+static func get_peer_id(data: Dictionary)-> int:
+	return data[KEY_PEER_ID]
+
+
+static func get_position(data: Dictionary)-> Vector3:
+	return data[KEY_POSITION]
+
+
+static func get_rotation(data: Dictionary)-> Vector3:
+	return data[KEY_ROTATION]
 
 
 static func get_timestamp(data: Dictionary)-> int:

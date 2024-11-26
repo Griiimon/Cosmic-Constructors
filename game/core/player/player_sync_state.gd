3b15e46007b0a83c39b0adf4381761f8a67e2401
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-26 11:40:38 +0100

    Update player_sync_state.gd

--- game/core/player/player_sync_state.gd
+++ game/core/player/player_sync_state.gd
@@ -19,9 +19,5 @@ static func parse_sync_state(player: BasePlayer, data: Dictionary):
 	player.global_rotation= data[KEY_ROTATION]
 
 
-static func get_peer_id(data: Dictionary)-> int:
-	return data[KEY_PEER_ID]
-
-
 static func get_timestamp(data: Dictionary)-> int:
 	return data[KEY_TIMESTAMP]

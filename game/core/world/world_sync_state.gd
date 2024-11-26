bb7b4de4f3db074edf5f3212daa2a43ecf31a3d2
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-26 16:03:19 +0100

    Update world_sync_state.gd

--- game/core/world/world_sync_state.gd
+++ game/core/world/world_sync_state.gd
@@ -4,5 +4,9 @@ const KEY_PLAYERS= "players"
 
 
 
-static func add_player_states(world_state: Dictionary, player_states: Dictionary):
+static func add_player_states(world_state: Dictionary, player_states: Array):
 	world_state[KEY_PLAYERS]= player_states
+
+
+static func parse_player_states(world_state: Dictionary)-> Array:
+	return world_state[KEY_PLAYERS]

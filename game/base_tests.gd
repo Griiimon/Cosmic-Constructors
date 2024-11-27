db32282eaf30d07aa57c29dcc0006dea0354323c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-27 10:07:36 +0100

    Update base_tests.gd

--- game/base_tests.gd
+++ game/base_tests.gd
@@ -57,6 +57,9 @@ func _input(event: InputEvent) -> void:
 				mine_gold()
 			elif event.keycode == KEY_F2:
 				player.world.freeze_grids(false)
+			elif event.keycode == KEY_F3:
+				if player:
+					player.wear_equipment(load("res://game/data/equipment/back/jetpack/atmospheric_jetpack.tres"))
 			elif event.keycode == KEY_F4:
 				DebugPanel.toggle()
 			elif event.keycode == KEY_F5:

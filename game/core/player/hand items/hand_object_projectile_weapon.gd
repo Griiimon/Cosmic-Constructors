beb1f85365bf969f66379c7e25d08122c111243b
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-27 11:57:34 +0100

    add recoil

--- game/core/player/hand items/hand_object_projectile_weapon.gd
+++ game/core/player/hand items/hand_object_projectile_weapon.gd
@@ -5,11 +5,13 @@ extends HandObject
 
 
 func use():
+	super()
+	
 	var weapon_definition: ProjectileWeapon= item_definition
 	var projectile: ProjectileObject= weapon_definition.projectile.scene.instantiate()
 	projectile.projectile_definition= weapon_definition.projectile
 	projectile.position= player.first_person_camera.project_position(player.item_camera.unproject_position(muzzle.global_position), 1.0)
-	#projectile.position= player.to_global(muzzle.global_position)
+
 	projectile.rotation= player.pivot.global_rotation
 	player.world.add_projectile(projectile)
 	projectile.velocity= -projectile.global_basis.z * weapon_definition.muzzle_velocity

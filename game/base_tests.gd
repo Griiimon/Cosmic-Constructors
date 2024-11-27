0d0846cc3c5ab4010f669398bb955fc9a4b331c0
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-27 11:44:50 +0100

    Update base_tests.gd

--- game/base_tests.gd
+++ game/base_tests.gd
@@ -62,7 +62,8 @@ func _input(event: InputEvent) -> void:
 				player.world.freeze_grids(false)
 			elif event.keycode == KEY_F3:
 				if player:
-					player.wear_equipment(load("res://game/data/equipment/back/jetpack/atmospheric_jetpack.tres"))
+					#player.wear_equipment(load("res://game/data/equipment/back/jetpack/atmospheric_jetpack.tres"))
+					player.play_animation("wave")
 			elif event.keycode == KEY_F4:
 				DebugPanel.toggle()
 			elif event.keycode == KEY_F5:

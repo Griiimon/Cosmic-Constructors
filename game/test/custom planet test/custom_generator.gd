e1da5e25d5a76c6cb8e93f8246ccae6682c89ed7
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-28 23:29:31 +0100

    improve custom planet

--- game/test/custom planet test/custom_generator.gd
+++ game/test/custom planet test/custom_generator.gd
@@ -1,8 +1,12 @@
+@tool
 class_name CustomVoxelGenerator
 extends VoxelGeneratorScript
 
 @export var height_noise: FastNoiseLite
-
+@export var height_factor: float= 100.0
+@export var mountains_noise: FastNoiseLite
+@export var mountains_threshold: float= 0.3
+@export var mountains_multiplier: float= 5.0
 
 # Change channel to SDF
 const channel : int = VoxelBuffer.CHANNEL_SDF
@@ -14,7 +18,13 @@ func _generate_block(out_buffer : VoxelBuffer, origin_in_voxels : Vector3i, lod 
 
 			var height := height_noise.get_noise_2d(pos_world.x, pos_world.z)
 			height*= height
-			height*= 100
+			height*= height_factor
+			
+			var mountains: float= mountains_noise.get_noise_2d(pos_world.x, pos_world.z)
+			if abs(mountains) > mountains_threshold:
+				var new_height= height * sign(mountains) * mountains_multiplier
+				height= lerp(height, new_height, clampf((abs(mountains) - mountains_threshold) * 10.0, 0.0, 1.0))
+			
 
 			for ry in out_buffer.get_size().y:
 				pos_world.y = origin_in_voxels.y + (ry << lod)

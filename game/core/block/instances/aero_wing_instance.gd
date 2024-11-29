67561684ebbabfe4b930cfa2c9abc04a6b6379ab
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-29 21:12:07 +0100

    add aero wing

---
+++ game/core/block/instances/aero_wing_instance.gd
@@ -0,0 +1,21 @@
+extends BlockInstance
+
+@onready var wing_joint: Node3D = %"Wing Joint"
+
+@onready var wing_angle: BlockPropFloat= BlockPropFloat.new("Wing Angle", 0.0, set_wing_angle).\
+	set_toggle_behavior(BlockPropFloat.ToggleBehavior.INCREASE).set_step_size(5.0).set_range(0.0, 25.0)
+
+
+
+func _ready() -> void:
+	default_interaction_property= wing_angle
+
+
+func physics_tick(grid: BlockGrid, grid_block: GridBlock, _delta: float):
+	var downforce: float= pow(grid.linear_velocity.length(), 2) * wing_angle.get_value_f() * 0.1
+	DebugHud.send("Downforce", "%.1f" % downforce)
+	grid.apply_force(-grid.global_basis.y * downforce, grid.get_global_block_pos(grid_block.local_pos) - grid.global_position)
+
+
+func set_wing_angle():
+	wing_joint.rotation_degrees.x= -wing_angle.get_value_f()

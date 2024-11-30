8fecc577fef9e04661ca77927b895c95545e6c08
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-30 12:30:46 +0100

    angle impact

--- game/core/block/instances/lift_wing_instance.gd
+++ game/core/block/instances/lift_wing_instance.gd
@@ -1,10 +1,12 @@
 extends BlockInstance
 
-@export var lift_factor: float= 10.0
+@export var lift_factor: float= 2.0
 
 
 
 func physics_tick(grid: BlockGrid, grid_block: GridBlock, _delta: float):
-	var lift: float= pow(max(-grid.get_local_velocity().z, 0.0), 2) * lift_factor
+	var velocity_impact: float= pow(max(-grid.get_local_velocity().z, 0.0), 2)
+	var angle_impact: float= pow(grid.linear_velocity.normalized().dot(-grid.global_basis.z), 2)
+	var lift: float= velocity_impact * angle_impact * lift_factor
 	DebugHud.send("Lift", "%.1f" % lift)
 	grid.apply_force(grid.global_basis.y * lift, grid.get_global_block_pos(grid_block.local_pos) - grid.global_position)

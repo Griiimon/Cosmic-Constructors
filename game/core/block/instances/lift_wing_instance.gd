dffa70f1bed244cdc21a2344e17be0d6fc63e972
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-11-30 10:50:52 +0100

    add lift wing

---
+++ game/core/block/instances/lift_wing_instance.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@export var lift_factor: float= 10.0
+
+
+
+func physics_tick(grid: BlockGrid, grid_block: GridBlock, _delta: float):
+	var lift: float= pow(max(-grid.get_local_velocity().z, 0.0), 2) * lift_factor
+	DebugHud.send("Lift", "%.1f" % lift)
+	grid.apply_force(grid.global_basis.y * lift, grid.get_global_block_pos(grid_block.local_pos) - grid.global_position)

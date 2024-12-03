b13d9e1e076b64afdde9b8e7f5c44c5c3314cadd
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-03 16:56:31 +0100

    free_children()

--- game/utils/utils.gd
+++ game/utils/utils.gd
@@ -11,3 +11,9 @@ static func align_with_y(xform: Transform3D, new_y: Vector3)-> Transform3D:
 	xform.basis.x = -xform.basis.z.cross(new_y)
 	xform.basis = xform.basis.orthonormalized()
 	return xform
+
+
+static func free_children(node: Node):
+	for child in node.get_children():
+		node.remove_child(child)
+		child.queue_free()

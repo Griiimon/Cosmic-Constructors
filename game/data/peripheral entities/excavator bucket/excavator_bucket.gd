71f94ffa49db3471594061fac5cf0a56fb52d93f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-03 09:11:39 +0100

    Update excavator_bucket.gd

--- game/data/peripheral entities/excavator bucket/excavator_bucket.gd
+++ game/data/peripheral entities/excavator bucket/excavator_bucket.gd
@@ -1,8 +1,8 @@
 extends RigidBody3D
-
-@onready var inner_rigidbody: RigidBody3D = $"Inner Rigidbody"
-
-
-
-func _ready() -> void:
-	inner_rigidbody.top_level= true
+#
+#@onready var inner_rigidbody: RigidBody3D = $"Inner Rigidbody"
+#
+#
+#
+#func _ready() -> void:
+	#inner_rigidbody.top_level= true

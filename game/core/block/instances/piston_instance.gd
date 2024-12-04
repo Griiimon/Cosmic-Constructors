67cf15ef3521716d3d3c2c1aa16105fc732a1225
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-04 18:10:37 +0100

    restoring and initial distance

--- game/core/block/instances/piston_instance.gd
+++ game/core/block/instances/piston_instance.gd
@@ -8,7 +8,7 @@ enum State { IDLE, MOVE }
 @onready var segments_node: Node3D = $Segments
 @onready var orig_segment: MeshInstance3D = $Segments/Segment
 
-@onready var joint: Generic6DOFJoint3D = $Generic6DOFJoint3D
+@onready var joint: JoltGeneric6DOFJoint3D = $JoltGeneric6DOFJoint3D
 
 @onready var velocity:= BlockPropFloat.new("Velocity", 1.0, change_velocity)
 
@@ -17,15 +17,12 @@ var sub_grid: BlockGrid
 
 var state: State= State.MOVE
 var segments: Array[Node3D]
-var extension: float= 0:
-	set(f):
-		if not is_equal_approx(f, extension):
-			extension= f
-			update_actual_velocity()
 
 var max_distance: float
 
 var actual_velocity: float
+
+var initial_distance: float
 
 
 
@@ -44,7 +41,7 @@ func _ready() -> void:
 	segments.assign(segments_node.get_children())
 
 	max_distance= segments.size()
-	joint.set_param_y(Generic6DOFJoint3D.PARAM_LINEAR_UPPER_LIMIT, max_distance)
+	joint.set_param_y(JoltGeneric6DOFJoint3D.PARAM_LINEAR_LIMIT_UPPER, max_distance)
 
 
 func on_placed(grid: BlockGrid, grid_block: GridBlock):
@@ -59,30 +56,55 @@ func on_placed(grid: BlockGrid, grid_block: GridBlock):
 
 
 func physics_tick(_grid: BlockGrid, _grid_block: GridBlock, _delta: float):
-	extension= global_position.distance_to(piston_head.global_position) - 1
-	DebugHud.send("Extension", extension)
+	var extension: float= get_joint_distance()
+	DebugHud.send("Extension", "%.2f" % extension)
 	
 	for i in segments.size():
 		segments[i].position.y= lerp(0.0, float(i + 1), extension / max_distance)
 
 
+func on_restored(grid: BlockGrid, grid_block: GridBlock, restore_data: Dictionary):
+	restore_grid_connection.call_deferred(grid, grid_block, restore_data["sub_grid_id"])
+	
+	
+func restore_grid_connection(grid: BlockGrid, grid_block: GridBlock, sub_grid_id: int):
+	sub_grid= grid.world.get_grid(sub_grid_id)
+	
+	joint.node_a= joint.get_path_to(grid)
+	joint.node_b= joint.get_path_to(sub_grid)
+
+	initial_distance= get_joint_distance()
+	#DebugHud.send("Init Hinge angle", int(rad_to_deg(initial_angle)))
+
+	on_set_active()
+
+
 func change_velocity():
-	#joint.set_param_y(Generic6DOFJoint3D.PARAM_LINEAR_MOTOR_TARGET_VELOCITY, velocity.get_value_f())
-	update_actual_velocity()
+	joint.set_param_y(JoltGeneric6DOFJoint3D.PARAM_LINEAR_MOTOR_TARGET_VELOCITY, velocity.get_value_f())
+
 
 func on_set_active():
-	update_actual_velocity()
-#	joint.set_flag_y(Generic6DOFJoint3D.FLAG_ENABLE_LINEAR_MOTOR, active.is_true())
+	joint.set_flag_y(JoltGeneric6DOFJoint3D.FLAG_ENABLE_LINEAR_MOTOR, active.is_true())
 
-
-func update_actual_velocity():
-	if not active.is_true():
-		actual_velocity= 0
+	if active.is_true():
+		joint.set_param_y(JoltGeneric6DOFJoint3D.PARAM_LINEAR_LIMIT_LOWER, 0 - initial_distance)
+		joint.set_param_y(JoltGeneric6DOFJoint3D.PARAM_LINEAR_LIMIT_UPPER, max_distance - initial_distance)
 	else:
-		var vel_f: float= velocity.get_value_f()
-		if (extension <= 0 and vel_f <= 0) or (extension >= max_distance and vel_f >= 0):
-			actual_velocity= 0
-		else:
-			actual_velocity= vel_f
+		while not joint.node_a:
+			await get_tree().physics_frame
 
-	joint.set_param_y(Generic6DOFJoint3D.PARAM_LINEAR_MOTOR_TARGET_VELOCITY, actual_velocity)
+		joint.set_param_y(JoltGeneric6DOFJoint3D.PARAM_LINEAR_MOTOR_TARGET_VELOCITY, 0)
+
+		var distance: float= get_joint_distance()
+		joint.set_param_y(JoltGeneric6DOFJoint3D.PARAM_LINEAR_LIMIT_LOWER, distance - initial_distance)
+		joint.set_param_y(JoltGeneric6DOFJoint3D.PARAM_LINEAR_LIMIT_UPPER, distance - initial_distance)
+
+
+func serialize()-> Dictionary:
+	var data: Dictionary= super()
+	data["sub_grid_id"]= sub_grid.get_id()
+	return data
+
+
+func get_joint_distance()-> float:
+	return global_position.distance_to(sub_grid.global_position) - 1

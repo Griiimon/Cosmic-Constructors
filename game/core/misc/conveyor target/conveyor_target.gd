5dfc533c2570bfa53470cb20c24a780541b08203
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-05 22:12:40 +0100

    Update conveyor_target.gd

--- game/core/misc/conveyor target/conveyor_target.gd
+++ game/core/misc/conveyor target/conveyor_target.gd
@@ -1,8 +1,19 @@
 class_name ConveyorTarget
 extends Node
 
-const NODE_NAME= "Conveyor Target"
+signal took_item(item_instance: WorldItemInstance)
 
+const NODE_NAME= "Conveyor Target"
+const CAN_TAKE_FUNCTION_NAME= "can_conveyor_target_take_item"
+
+
+func can_take_item(item: WorldItem)-> bool:
+	assert(get_parent().has_method(CAN_TAKE_FUNCTION_NAME))
+	return get_parent().call(CAN_TAKE_FUNCTION_NAME)
+
+
+func take_item(item_instance: WorldItemInstance):
+	took_item.emit(item_instance)
 
 
 static func get_target_from_block_pos(grid: BlockGrid, block_pos: Vector3i)-> ConveyorTarget:

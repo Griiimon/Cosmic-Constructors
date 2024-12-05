57ede93bcfefcd7f6ac2e61fa10cf7f56fb775b7
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-05 20:31:21 +0100

    Update furnace_instance.gd

--- game/core/block/instances/furnace_instance.gd
+++ game/core/block/instances/furnace_instance.gd
@@ -1,3 +1,19 @@
-extends BlockInstance
+extends BlockInstanceOnOff
+
+@export var recipe: FurnaceRecipe
 
 @onready var fire_particles: CPUParticles3D = $CPUParticles3D
+@onready var ejector: ItemEjector = $"Item Ejector"
+
+var progress: float= 0.0
+
+
+
+func physics_tick(_grid: BlockGrid, _grid_block: GridBlock, delta: float):
+	if progress < 1:
+		progress+= delta
+	
+	if progress >= 1:
+		if ejector.can_eject():
+			ejector.eject_item(recipe.product)
+			progress= 0

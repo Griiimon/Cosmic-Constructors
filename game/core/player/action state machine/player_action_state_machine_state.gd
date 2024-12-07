3270add76a4b4626d7495cafe173f52a0779323f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-07 14:13:21 +0100

    move ghost functions to base class

---
+++ game/core/player/action state machine/player_action_state_machine_state.gd
@@ -0,0 +1,57 @@
+class_name PlayerActionStateMachineState
+extends PlayerStateMachineState
+
+const KEEP_GHOST_GROUP= "Keep Ghost"
+
+var ghost: Node3D
+
+
+
+func init_ghost(model: Node3D):
+	
+	if ghost:
+		remove_ghost()
+
+	ghost= model
+	ghost.set_script(null)
+	add_child(ghost)
+	ghost.top_level= true
+	
+	var mesh_instances: Array= ghost.find_children("*")
+	if ghost is MeshInstance3D:
+		mesh_instances.append(ghost)
+	
+	for child in mesh_instances:
+		if child is not MeshInstance3D and not child.is_in_group(KEEP_GHOST_GROUP):
+			child.queue_free()
+		else:
+			if child is not MeshInstance3D: continue
+			
+			var mesh_instance: MeshInstance3D= child
+			var old_material: Material= mesh_instance.mesh.surface_get_material(0)
+			var new_material: StandardMaterial3D
+			if not old_material or old_material is ShaderMaterial:
+				new_material= StandardMaterial3D.new()
+			else:
+				new_material= old_material.duplicate()
+			
+			new_material.albedo_color= Color.SKY_BLUE
+			new_material.albedo_color.a= 0.7
+			
+			new_material.metallic= 0
+			new_material.roughness= 1
+			
+			mesh_instance.mesh= mesh_instance.mesh.duplicate()
+			mesh_instance.mesh.surface_set_material(0, new_material)
+			
+			#material.transparency= BaseMaterial3D.TRANSPARENCY_ALPHA
+			#material.albedo_color.a= 0.8
+
+	ghost.hide()
+
+
+func remove_ghost():
+	if not ghost: return
+	remove_child(ghost)
+	ghost.queue_free()
+	ghost= null
--- game/core/player/action state machine/player_build_state.gd
+++ game/core/player/action state machine/player_build_state.gd
@@ -1,7 +1,6 @@
 class_name PlayerBuildState
-extends PlayerStateMachineState
+extends PlayerActionStateMachineState
 
-const KEEP_GHOST_GROUP= "Keep Ghost"
 
 @export var build_range: float= 4.0
 @export var smooth_rotation_speed: float= 2.0
@@ -9,9 +8,9 @@ const KEEP_GHOST_GROUP= "Keep Ghost"
 	set(b):
 		current_block= b
 		if not is_inside_tree(): return
-		init_ghost()
+		if current_block:
+			init_ghost(current_block.get_model())
 
-var ghost: Node3D
 var grid: BlockGrid
 var local_block_pos: Vector3i
 var block_rotation: Vector3i
@@ -22,62 +21,12 @@ func on_enter():
 	player.build_raycast.enabled= true
 	player.build_raycast.target_position.z= -build_range
 	
-	init_ghost()
+	if current_block:
+		init_ghost(current_block.get_model())
 
 
 func on_exit():
 	remove_ghost()
-	
-
-func init_ghost():
-	if not current_block: return
-	
-	if ghost:
-		remove_ghost()
-		
-	ghost= current_block.get_model()
-	ghost.set_script(null)
-	add_child(ghost)
-	ghost.top_level= true
-	
-	var mesh_instances: Array= ghost.find_children("*")
-	if ghost is MeshInstance3D:
-		mesh_instances.append(ghost)
-	
-	for child in mesh_instances:
-		if child is not MeshInstance3D and not child.is_in_group(KEEP_GHOST_GROUP):
-			child.queue_free()
-		else:
-			if child is not MeshInstance3D: continue
-			
-			var mesh_instance: MeshInstance3D= child
-			var old_material: Material= mesh_instance.mesh.surface_get_material(0)
-			var new_material: StandardMaterial3D
-			if not old_material or old_material is ShaderMaterial:
-				new_material= StandardMaterial3D.new()
-			else:
-				new_material= old_material.duplicate()
-			
-			new_material.albedo_color= Color.SKY_BLUE
-			new_material.albedo_color.a= 0.7
-			
-			new_material.metallic= 0
-			new_material.roughness= 1
-			
-			mesh_instance.mesh= mesh_instance.mesh.duplicate()
-			mesh_instance.mesh.surface_set_material(0, new_material)
-			
-			#material.transparency= BaseMaterial3D.TRANSPARENCY_ALPHA
-			#material.albedo_color.a= 0.8
-
-	ghost.hide()
-
-
-func remove_ghost():
-	if not ghost: return
-	remove_child(ghost)
-	ghost.queue_free()
-	ghost= null
 
 
 func on_physics_process(delta: float):

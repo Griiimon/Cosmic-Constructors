04268ce26260a0f04b493bf0f94af5e7af26f466
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-08 12:00:13 +0100

    move files

--- game/core/components/item ejector/item_ejector.gd
+++ game/core/components/item ejector/item_ejector.gd
@@ -6,17 +6,19 @@ const NODE_NAME= "Item Ejector"
 @export var only_with_valid_target: bool= true
 
 @onready var raycast: RayCast3D = $RayCast3D
+@onready var item_spawn_position: Marker3D = $"Item Spawn Position"
+@onready var cooldown: Timer = $Cooldown
 
 
 
-func eject_item(item: WorldItem):
+func eject_item(item: WorldItem, world: World):
 	if only_with_valid_target:
 		var catcher: ItemCatcher= get_item_catcher()
 		assert(catcher)
 		catcher.catch(item)
-			
 	else:
-		assert(false, "not implemented")
+		world.spawn_item(item, item_spawn_position.global_position, item_spawn_position.global_rotation)
+	cooldown.start()
 
 
 func get_item_catcher()-> ItemCatcher:
@@ -35,4 +37,4 @@ func get_item_catcher()-> ItemCatcher:
 
 
 func can_eject()-> bool:
-	return not only_with_valid_target or get_item_catcher() != null
+	return cooldown.is_stopped() and ( not only_with_valid_target or get_item_catcher() != null )

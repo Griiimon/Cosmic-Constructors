fed5901bc49d185954550be2aaec999b66eec3a5
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-09 08:12:46 +0100

    move all minig logic to mine()

--- game/core/world/terrain/my_terrain.gd
+++ game/core/world/terrain/my_terrain.gd
@@ -14,17 +14,26 @@ func _ready() -> void:
 	assert(world != null)
 
 
-func mine(local_pos: Vector3i, radius: float, material_spawn_pos: Vector3, item_impulse: Vector3):
+func mine(local_pos: Vector3i, radius: float, spawn_items: bool= false, material_spawn_pos: Vector3= Vector3.ZERO, item_impulse: Vector3= Vector3.ZERO)-> Dictionary:
+	VoxelUtils.pre_mine(self, local_pos, radius)
+		
+	var tool: VoxelToolLodTerrain= get_voxel_tool()
+	tool.channel= VoxelBuffer.CHANNEL_SDF
+	tool.mode= VoxelTool.MODE_REMOVE
+	tool.do_sphere(local_pos, radius)
+	
 	var new_resources: Dictionary= VoxelUtils.mined(self, local_pos, radius)
 
 	var collected_resources: Dictionary
 	
 	for key in new_resources.keys():
-		if not collected_resources.has(key):
-			collected_resources[key]= 0.0
-		collected_resources[key]+= new_resources[key]
 		var raw_item: RawItem= terrain_properties.types[key].raw_material
-		var item_instance: WorldItemInstance= world.spawn_item(raw_item, material_spawn_pos)
-		item_instance.linear_velocity= item_impulse
-	#DebugHud.send("Gold", collected_resources[1] if collected_resources.has(1) else 0.0)	
-	DebugHud.send("Ress", collected_resources)
+		assert(not collected_resources.has(raw_item))
+		collected_resources[raw_item]= new_resources[key]
+
+		if spawn_items:
+			var item_instance: WorldItemInstance= world.spawn_item(raw_item, material_spawn_pos)
+			item_instance.linear_velocity= item_impulse
+	
+	#DebugHud.send("Ress", collected_resources)
+	return collected_resources

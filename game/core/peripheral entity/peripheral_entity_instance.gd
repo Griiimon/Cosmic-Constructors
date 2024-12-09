eba1cf082814b3ad65fc4776d661bf55fa9c7316
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-09 11:46:49 +0100

    auto set collision info

--- game/core/peripheral entity/peripheral_entity_instance.gd
+++ game/core/peripheral entity/peripheral_entity_instance.gd
@@ -1,2 +1,13 @@
 class_name PeripheralEntityInstance
 extends RigidBody3D
+
+
+
+func _ready() -> void:
+	collision_layer= Global.PERIPHERAL_GRID_ENTITIES_COLLISION_LAYER
+	
+	collision_mask= Global.PLAYER_COLLISION_LAYER +\
+		Global.GRID_COLLISION_LAYER +\
+		Global.TERRAIN_COLLISION_LAYER +\
+		Global.PHYSICAL_OBJECTS_COLLISION_LAYER +\
+		Global.PERIPHERAL_GRID_ENTITIES_COLLISION_LAYER

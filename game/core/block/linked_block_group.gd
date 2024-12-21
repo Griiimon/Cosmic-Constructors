794225cf3ecf4c39d5e8826b79fb0179bb37e01e
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-22 00:24:12 +0100

    Update linked_block_group.gd

--- game/core/block/linked_block_group.gd
+++ game/core/block/linked_block_group.gd
@@ -2,6 +2,28 @@ class_name LinkedBlockGroup
 
 const FLUID_CONTENT_KEY= "fluid"
 const GAS_CONTENT_KEY= "gas"
-const SUB_GRID_ID= "sub_grid"
+const SUB_GRID_ID_KEY= "sub_grid"
+const BLOCKS_KEY= "blocks"
 
+static var NEXT_ID: int= 0
+
+var id: int
 var data: Dictionary
+var persistent: bool
+
+
+
+func _init(_persistent: bool):
+	persistent= _persistent
+	id= NEXT_ID
+	NEXT_ID+= 1
+
+
+func register_block(grid_block: GridBlock, value: Variant= 0):
+	if not data.has(BLOCKS_KEY):
+		data[BLOCKS_KEY]= {}
+	set_block(grid_block, value)
+
+
+func set_block(grid_block: GridBlock, value: Variant):
+	data[BLOCKS_KEY][grid_block.local_pos]= value

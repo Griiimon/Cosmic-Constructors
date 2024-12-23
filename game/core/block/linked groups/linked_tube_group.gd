da04ebe1dc5fa5a39b4f4e2c5c0f30ebaa1ca10c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-23 01:44:18 +0100

    Update linked_tube_group.gd

--- game/core/block/linked groups/linked_tube_group.gd
+++ game/core/block/linked groups/linked_tube_group.gd
@@ -3,3 +3,14 @@ extends LinkedBlockGroup
 
 var inputs: Array[FluidContainer]
 var outputs: Array[FluidConsumer]
+
+
+
+func add_input(input: FluidContainer):
+	if not input in inputs:
+		inputs.append(input)
+
+
+func add_output(output: FluidConsumer):
+	if not output in outputs:
+		outputs.append(output)

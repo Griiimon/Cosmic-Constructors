ca6a783888ee2a230a99e5cef779788cfcdfa415
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-23 09:32:17 +0100

    remove group

--- game/core/block/instances/engine_instance.gd
+++ game/core/block/instances/engine_instance.gd
@@ -2,8 +2,6 @@ extends BlockInstanceOnOff
 
 @onready var model: Node3D = $Model
 
-var engine_block_group: LinkedBlockGroup
-
 
 
 func _ready() -> void:
@@ -11,17 +9,7 @@ func _ready() -> void:
 	active.set_true()
 
 
-func on_placed(grid: BlockGrid, grid_block: GridBlock):
-	engine_block_group= find_or_make_linked_block_group(grid, grid_block.local_pos, false)
-	engine_block_group.register_block(grid_block)
-
-
 func physics_tick(grid: BlockGrid, grid_block: GridBlock, _delta: float):
 	var torque_output: float= 0.0
 	if active.is_true():
 		torque_output= grid.get_throttle_input() * (grid_block.block_definition as EngineBlock).torque_factor
-	engine_block_group.set_block(grid_block, torque_output)
-
-
-func get_linked_block_group()-> LinkedBlockGroup:
-	return engine_block_group

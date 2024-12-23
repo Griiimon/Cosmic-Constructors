d9f3c6b3072c49f89837f07fcb4f04576bb652dc
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-23 10:27:37 +0100

    consume fuel

--- game/core/block/instances/engine_instance.gd
+++ game/core/block/instances/engine_instance.gd
@@ -1,15 +1,21 @@
 extends BlockInstanceOnOff
 
-@onready var model: Node3D = $Model
+@export var max_fuel_consumption: float= 1.0
 
+@onready var model: Node3D = $Model
+@onready var fluid_consumer: FluidConsumer = $"Fluid Consumer"
+
+var current_throttle_input: float
 
 
 func _ready() -> void:
 	default_interaction_property= active
 	active.set_true()
+	fluid_consumer.variable_consumption= func(): return current_throttle_input * max_fuel_consumption
 
 
 func physics_tick(grid: BlockGrid, grid_block: GridBlock, _delta: float):
 	var torque_output: float= 0.0
 	if active.is_true():
-		torque_output= grid.get_throttle_input() * (grid_block.block_definition as EngineBlock).torque_factor
+		current_throttle_input= grid.get_throttle_input()
+		torque_output= current_throttle_input * (grid_block.block_definition as EngineBlock).torque_factor

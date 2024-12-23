f1afeba2642e351a858f993830109a5278f02352
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-23 23:07:42 +0100

    Update funnel_instance.gd

--- game/core/block/instances/funnel_instance.gd
+++ game/core/block/instances/funnel_instance.gd
@@ -1,3 +1,8 @@
 extends BlockInstance
 
 @onready var gather_area: Area3D = $"Gather Area"
+@onready var fluid_container: FluidContainer = $"Fluid Container"
+
+
+func _on_gather_area_body_entered(body: Node3D) -> void:
+	fluid_container.fill(1)

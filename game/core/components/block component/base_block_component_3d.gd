d6643c374d6a2940aa61286233b35b9c2dfb0bce
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-23 01:08:35 +0100

    fix return types

--- game/core/components/block component/base_block_component_3d.gd
+++ game/core/components/block component/base_block_component_3d.gd
@@ -3,19 +3,19 @@ extends Node3D
 
 
 
-static func get_from_block_pos(grid: BlockGrid, block_pos: Vector3i, node_name: String)-> BaseBlockComponent:
+static func get_from_block_pos(grid: BlockGrid, block_pos: Vector3i, node_name: String)-> BaseBlockComponent3D:
 	var block: BaseGridBlock= grid.get_block_local(block_pos)
 	if block:
 		return get_from_block(block, node_name)
 	return null
 
 
-static func get_from_block(block: BaseGridBlock, node_name: String)-> BaseBlockComponent:
+static func get_from_block(block: BaseGridBlock, node_name: String)-> BaseBlockComponent3D:
 	var block_instance: BlockInstance= block.get_block_instance()
 	if block_instance:
 		return get_from_node(block_instance, node_name)
 	return null
 
 
-static func get_from_node(node: Node, node_name: String)-> BaseBlockComponent:
+static func get_from_node(node: Node, node_name: String)-> BaseBlockComponent3D:
 	return node.get_node_or_null(node_name)

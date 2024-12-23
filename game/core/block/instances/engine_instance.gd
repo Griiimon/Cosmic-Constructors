da7b68048cdb9343c210fba06cae614f39812311
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-23 10:41:41 +0100

    add animation

--- game/core/block/instances/engine_instance.gd
+++ game/core/block/instances/engine_instance.gd
@@ -6,6 +6,8 @@ extends BlockInstanceOnOff
 @onready var fluid_consumer: FluidConsumer = $"Fluid Consumer"
 
 var current_throttle_input: float
+var tween: Tween
+
 
 
 func _ready() -> void:
@@ -19,3 +21,13 @@ func physics_tick(grid: BlockGrid, grid_block: GridBlock, _delta: float):
 	if active.is_true():
 		current_throttle_input= grid.get_throttle_input()
 		torque_output= current_throttle_input * (grid_block.block_definition as EngineBlock).torque_factor
+
+	if torque_output > 0:
+		if not tween:
+			tween= create_tween()
+			tween.tween_property(model, "position.y", 0.1, 0.1)
+			tween.tween_property(model, "position.y", 0.0, 0.1)
+			tween.set_loops()
+	else:
+		if tween:
+			tween.kill()

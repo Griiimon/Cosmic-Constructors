fd0ae2203c01764434654c7aa0e5611c3a230b57
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-23 23:07:52 +0100

    Update linked_tube_group.gd

--- game/core/block/linked groups/linked_tube_group.gd
+++ game/core/block/linked groups/linked_tube_group.gd
@@ -5,8 +5,8 @@ var inputs: Array[FluidContainer]
 var outputs: Array[FluidConsumer]
 
 var cached_input_ratio: Dictionary
-
 var can_currently_provide: float
+var fill_containers: bool
 
 
 
@@ -18,12 +18,19 @@ func tick(_delta: float):
 func process_input():
 	can_currently_provide= 0
 	cached_input_ratio= {}
+	fill_containers= false
+
 
 	for i in inputs.size():
 		var input: FluidContainer= inputs[i]
 		
-		can_currently_provide+= input.content
-		cached_input_ratio[i]= input.content
+		if input.keep_empty: 
+			fill_containers= true
+			continue
+		
+		var max_output: float= min(input.throughput, input.content)
+		can_currently_provide+= max_output
+		cached_input_ratio[i]= max_output
 
 	if is_zero_approx(can_currently_provide): return
 
@@ -41,27 +48,57 @@ func process_output():
 		total_requested+= consumption
 		output_ratios[i]= consumption
 	
-	if is_zero_approx(total_requested): return
+	if total_requested > 0:
 	
-	if can_currently_provide >= total_requested:
-		for key in output_ratios.keys():
-			output_ratios[key]= 1.0
-	else:
-		for key in output_ratios.keys():
-			output_ratios[key]/= total_requested
-	
-	total_requested= max(total_requested, can_currently_provide)
-	
-	for i in outputs.size():
-		var output: FluidConsumer= outputs[i]
-		output.supply_ratio(output_ratios[i])
-	
-	drain(total_requested)
+		if can_currently_provide >= total_requested:
+			for key in output_ratios.keys():
+				output_ratios[key]= 1.0
+		else:
+			for key in output_ratios.keys():
+				output_ratios[key]/= total_requested
+		
+		total_requested= max(total_requested, can_currently_provide)
+		
+		for i in outputs.size():
+			var output: FluidConsumer= outputs[i]
+			output.supply(output_ratios[i] * total_requested)
+		
+		drain(total_requested)
+
+	if fill_containers and total_requested < can_currently_provide:
+		total_requested= can_currently_provide - total_requested
+		var fill_capacities: Dictionary
+		var total_fill_capacity: float
+		
+		for input in inputs:
+			if input.keep_empty: continue
+			var fill_capacity: float= min(input.throughput, input.get_empty_capacity())
+			total_fill_capacity+= fill_capacity
+			fill_capacities[input]= fill_capacity
+		
+		var fill_ratios: Dictionary
+		if total_fill_capacity > 0:
+			if total_fill_capacity < total_requested:
+				for input in fill_ratios.keys():
+					fill_ratios[input]= 1.0
+			else:
+				for input in fill_ratios.keys():
+					fill_ratios[input]= fill_capacities[input] / total_fill_capacity
+			
+			total_requested= min(total_requested, total_fill_capacity)
+			
+			for input: FluidContainer in fill_ratios.keys():
+				input.fill(fill_ratios[input] * total_requested)
+			
+			drain(total_requested, true)
 
 
-func drain(amount: float):
+func drain(amount: float, forced: bool= false):
 	for i in inputs.size():
 		var input: FluidContainer= inputs[i]
+		
+		if forced and not input.keep_empty: continue
+		
 		# TODO check if all requested was actually available to be drained
 		input.drain(amount * cached_input_ratio[i])
 

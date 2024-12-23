3489b650c760e56ae3d8b4737624c8598f4a3d71
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-23 01:44:13 +0100

    extend FluidBlockComponent

--- game/core/components/fluid container/fluid_container.gd
+++ game/core/components/fluid container/fluid_container.gd
@@ -1,26 +1,9 @@
 @tool
 class_name FluidContainer
-extends BaseBlockComponent3D
+extends FluidBlockComponent
 
-@export var connectors: Array[FluidConnector]
+const NODE_NAME= "Fluid Container"
 
-@export var connector_model: PackedScene
-@export var model_node: Node3D
-
-@export var add_connectors_to_model: bool= false:
-	set(b):
-		if b:
-			var ctr:= 0
-			for connector in connectors:
-				var connector_instance: Node3D= connector_model.instantiate()
-				connector_instance.position= Vector3(connector.block_pos) + connector.direction * 0.5
-				connector_instance.rotation= Quaternion(Vector3.FORWARD, Vector3(connector.direction)).get_euler()
-				model_node.add_child(connector_instance)
-				connector_instance.owner= get_parent()
-				connector_instance.name= str("Fluid Connector ", ctr + 1)
-				ctr+= 1
-
-var fluid: Fluid
 var content: float
 var max_storage: float
 

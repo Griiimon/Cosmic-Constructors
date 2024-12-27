c6186f5731f8d3426e790cf9063abec287b78305
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-27 21:34:37 +0100

    move interaction_logic()

--- game/core/player/action state machine/player_action_idle_state.gd
+++ game/core/player/action state machine/player_action_idle_state.gd
@@ -64,22 +64,9 @@ func on_physics_process(_delta: float):
 						property.toggle()
 						SignalManager.block_property_changed.emit(property)
 						return
-	
-	
-	if Input.is_action_just_pressed("interact"):
-		var shapecast: ShapeCast3D= player.interact_shapecast
-		if shapecast.is_colliding():
-			grid= shapecast.get_collider(0)
-			assert(grid)
-			var collision_pos: Vector3= shapecast.get_collision_point(0)
-			collision_pos-= shapecast.global_basis.z * 0.01
-			
-			grid_block= grid.get_block_from_global_pos(collision_pos)
-			if grid_block:
-				if grid_block.get_block_definition().can_interact() and grid_block.get_block_instance().can_interact(grid, grid_block, player):
-					grid_block.get_block_instance().interact(grid, grid_block, player)
-					return
 
+	interaction_logic()
+	
 
 func equip_hand_item(hand_item: HandItem):
 	force_unequip_item()
--- game/core/player/action state machine/player_action_state_machine_state.gd
+++ game/core/player/action state machine/player_action_state_machine_state.gd
@@ -7,6 +7,22 @@ var ghost: Node3D
 
 
 
+func interaction_logic():
+	if Input.is_action_just_pressed("interact"):
+		var shapecast: ShapeCast3D= player.interact_shapecast
+		if shapecast.is_colliding():
+			var grid: BlockGrid= shapecast.get_collider(0)
+			assert(grid)
+			var collision_pos: Vector3= shapecast.get_collision_point(0)
+			collision_pos-= shapecast.global_basis.z * 0.01
+			
+			var grid_block: GridBlock= grid.get_block_from_global_pos(collision_pos)
+			if grid_block:
+				if grid_block.get_block_definition().can_interact() and grid_block.get_block_instance().can_interact(grid, grid_block, player):
+					grid_block.get_block_instance().interact(grid, grid_block, player)
+					return
+
+
 func init_ghost(model: Node3D):
 	
 	if ghost:

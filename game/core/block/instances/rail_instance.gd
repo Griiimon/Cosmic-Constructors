52820d598f1b644d37b91c03ca7e233e18ffe923
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-29 21:41:35 +0100

    Update rail_instance.gd

--- game/core/block/instances/rail_instance.gd
+++ game/core/block/instances/rail_instance.gd
@@ -12,7 +12,9 @@ func on_placed(grid: BlockGrid, grid_block: GridBlock):
 
 	if not group:
 		rail= LinkedRailGroup.new(grid)
-		rail.sub_grid= grid.world.add_custom_grid(CarriageBlockGrid.new(), global_position + global_basis.y, global_basis.rotation)
+		rail.sub_grid= grid.world.add_custom_grid(CarriageBlockGrid.new(), global_position + global_basis.y, global_rotation)
+		rail.sub_grid.rail= rail
+		rail.sub_grid.add_block(carriage_block, Vector3i.ZERO)
 	else:
 		assert(group is LinkedRailGroup)
 		rail= group
@@ -29,6 +31,9 @@ func on_destroy(grid: BlockGrid, grid_block: GridBlock):
 	super(grid, grid_block)
 
 
-func block_group_filter(grid: BlockGrid, block_pos: Vector3i, neighbor_pos: Vector3i)-> bool:
-	var grid_block: GridBlock= grid.get_block_local(block_pos)
+func block_group_filter(grid: BlockGrid, grid_block: GridBlock, neighbor_pos: Vector3i)-> bool:
 	return grid_block.to_global(Vector3i.FORWARD) == neighbor_pos or grid_block.to_global(Vector3i.BACK) == neighbor_pos
+
+
+func get_linked_block_group()-> LinkedBlockGroup:
+	return rail

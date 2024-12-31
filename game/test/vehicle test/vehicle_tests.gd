3db53ac369e96bd13e23256e582fe8bd83b5c1c0
Griiimon <25064539+Griiimon@users.noreply.github.com>
2024-12-31 13:34:19 +0100

    Update vehicle_tests.gd

--- game/test/vehicle test/vehicle_tests.gd
+++ game/test/vehicle test/vehicle_tests.gd
@@ -8,3 +8,6 @@ extends BaseTests
 func on_start():
 	for grid: BlockGrid in player.world.grids.get_children():
 		grid.current_gravity= Vector3(0, -10, 0)
+
+	var item= load("res://game/data/items/raw materials/iron ore/iron_ore.tres")
+	player.world.spawn_item(item, Vector3(0, 1, -5))

3f84e6f7cc473161e34143f9accc67d85572d789
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-06 12:35:30 +0100

    add fixed wheel block

---
+++ game/core/block/instances/fixed_wheel_instance.gd
@@ -0,0 +1,15 @@
+extends BlockInstance
+
+@export var wheel_scene: PackedScene
+
+@onready var joint: Generic6DOFJoint3D = $Generic6DOFJoint3D
+
+
+
+func on_placed(grid: BlockGrid, _grid_block: GridBlock):
+	var wheel: RigidBody3D= wheel_scene.instantiate()
+	joint.add_child(wheel)
+	wheel.top_level= true
+
+	joint.node_a= joint.get_path_to(grid)
+	joint.node_b= joint.get_path_to(wheel)

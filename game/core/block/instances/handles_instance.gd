c93a6d3df0dea7f17c9bbc0fd77d760cacf065e6
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-06 23:56:27 +0100

    fix handles

--- game/core/block/instances/handles_instance.gd
+++ game/core/block/instances/handles_instance.gd
@@ -1,10 +1,13 @@
 class_name HandlesInstance
 extends BlockInstance
 
+@export var handles_rigidbody_scene: PackedScene
+@export var alignment_factor: float= 0.5
+
 @onready var joint: JoltGeneric6DOFJoint3D = $JoltGeneric6DOFJoint3D
-@onready var rigidbody: RigidBody3D = $RigidBody3D
 
 var grabbed_by_player: Player
+var rigidbody: RigidBody3D
 
 
 
@@ -16,13 +19,14 @@ func interact(grid: BlockGrid, grid_block: GridBlock, player: Player):
 
 func physics_tick(_grid: BlockGrid, _grid_block: GridBlock, _delta: float):
 	if grabbed_by_player:
-		if not rigidbody.freeze:
+		if rigidbody:
 			var hold_position: Vector3= grabbed_by_player.handle_grabber.global_position
 			var vec: Vector3= hold_position - rigidbody.global_position
 			#rigidbody.linear_velocity= vec.normalized() * pow(vec.length() * 2, 2)
 			rigidbody.linear_velocity= vec * 5
-			var quat: Quaternion= grabbed_by_player.handle_grabber.global_basis.get_rotation_quaternion() * rigidbody.global_basis.get_rotation_quaternion().inverse()
-			rigidbody.apply_torque(quat.get_axis() * quat.get_angle() * 0.1)
+			#var quat: Quaternion= grabbed_by_player.handle_grabber.global_basis.get_rotation_quaternion() * rigidbody.global_basis.get_rotation_quaternion().inverse()
+			#rigidbody.apply_torque(quat.get_axis() * quat.get_angle() * 0.1)
+			rigidbody.apply_torque(Utils.calc_angular_velocity(rigidbody.global_basis, grabbed_by_player.handle_grabber.global_basis) * alignment_factor)
 
 
 func update_joint(grid: BlockGrid, player: Player):
@@ -31,14 +35,17 @@ func update_joint(grid: BlockGrid, player: Player):
 	if player.is_rigidbody():
 		joint.node_b= joint.get_path_to(player)
 	else:
-		#joint.node_b= joint.get_path_to(rigidbody)
-		#joint.set_flag_y(JoltGeneric6DOFJoint3D.FLAG_ENABLE_ANGULAR_LIMIT, false)
-		rigidbody.top_level= true
-		rigidbody.freeze= false
+		rigidbody= handles_rigidbody_scene.instantiate()
+		rigidbody.position= joint.global_position
+		#rigidbody.rotation= grabbed_by_player.handle_grabber.global_rotation
+		rigidbody.rotation= global_rotation
+		joint.add_child(rigidbody)
+		joint.node_b= joint.get_path_to(rigidbody)
 		
 
 func disconnect_joint():
 	joint.node_a= ""
 	joint.node_b= ""
-	rigidbody.freeze= true
-	joint.set_flag_y(JoltGeneric6DOFJoint3D.FLAG_ENABLE_ANGULAR_LIMIT, true)
+	if rigidbody:
+		rigidbody.queue_free()
+		rigidbody= null

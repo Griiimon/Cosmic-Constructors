9db4aafb78b9fbc0d1031bd9db8c407dccb5391d
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-07 22:47:27 +0100

    include grid

--- game/core/misc/hotkey assignment/hotkey_assignment_block_property.gd
+++ game/core/misc/hotkey assignment/hotkey_assignment_block_property.gd
@@ -3,39 +3,52 @@ extends BaseHotkeyAssignment
 
 var block_pos: Vector3i
 var property_name: String
+var grid_id: int
 
 
 
-func _init(_key: int, block: GridBlock= null, property: BlockProperty= null):
+func _init(_key: int, block: GridBlock= null, property: BlockProperty= null, grid: BlockGrid= null):
+	assert((block and property and grid) or (not block and not property and not grid))
 	super(_key)
 	if block:
 		block_pos= block.local_pos
 	if property:
 		property_name= property.display_name
+	if grid:
+		grid_id= grid.get_id()
 
 
 func serialize()-> Dictionary:
-	return { "assignment_type" : (get_script() as Script).get_global_name(), "block_pos" : block_pos, "property" : property_name }
+	return { "assignment_type" : (get_script() as Script).get_global_name(),\
+	 "block_pos" : block_pos,\
+	 "property" : property_name,\
+	 "grid_id" : grid_id }
 
 
 static func deserialize(key: int, data: Dictionary)-> HotkeyAssignmentBlockProperty:
 	var inst:= HotkeyAssignmentBlockProperty.new(key, null, null)
 	inst.block_pos= str_to_var("Vector3i" + data["block_pos"])
 	inst.property_name= data["property"]
+	inst.grid_id= data["grid_id"]
 	return inst
 
 
-func get_block(grid: BlockGrid)-> GridBlock:
-	return grid.get_block_local(block_pos)
+func get_block(caller_grid: BlockGrid)-> GridBlock:
+	return get_grid(caller_grid.world).get_block_local(block_pos)
 
 
-func get_property(grid: BlockGrid)-> BlockProperty:
-	var block: GridBlock= get_block(grid)
+func get_property(caller_grid: BlockGrid)-> BlockProperty:
+	var block: GridBlock= get_block(get_grid(caller_grid.world))
 	if not block: return null
 	return block.get_block_instance().property_table[property_name]
 	
 
-func get_as_text(grid: BlockGrid= null)-> String:
+func get_as_text(caller_grid: BlockGrid= null)-> String:
+	var grid: BlockGrid= get_grid(caller_grid.world)
 	var block: GridBlock= get_block(grid)
 	if not block: return ""
 	return str(block.get_name(grid), "\n", property_name)
+
+
+func get_grid(world: World)-> BlockGrid:
+	return world.get_grid(grid_id)

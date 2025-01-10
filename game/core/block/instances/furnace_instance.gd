562d99dce8104c3e8b394211a897926ff8a081f1
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-10 02:12:25 +0100

    add ItemCatcher

--- game/core/block/instances/furnace_instance.gd
+++ game/core/block/instances/furnace_instance.gd
@@ -4,11 +4,16 @@ extends BlockInstanceOnOff
 
 @onready var fire_particles: CPUParticles3D = $CPUParticles3D
 @onready var ejector: ItemEjector = $"Item Ejector"
+@onready var item_catcher: ItemCatcher = $"Item Catcher"
 
 var progress: float= 0.0
 
 
 
+func _ready() -> void:
+	item_catcher.caught_item.connect(on_add_ingredient)
+	
+	
 func physics_tick(grid: BlockGrid, _grid_block: GridBlock, delta: float):
 	if progress < 1:
 		progress+= delta
@@ -17,3 +22,7 @@ func physics_tick(grid: BlockGrid, _grid_block: GridBlock, delta: float):
 		if ejector.can_eject():
 			ejector.eject_item(InventoryItem.new(recipe.product), grid.world)
 			progress= 0
+
+
+func on_add_ingredient(inv_item: InventoryItem):
+	

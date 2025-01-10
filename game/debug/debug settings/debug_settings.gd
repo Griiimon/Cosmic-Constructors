9bfa694c252717072d45c06407f9c06a8f8872be
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-10 04:59:28 +0100

    add DebugSettings

---
+++ game/debug/debug settings/debug_settings.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var default_build_hotbar_layout: Array[Block]
+
+
+
+func _ready() -> void:
+	if not is_enabled(): return
+	SignalManager.player_spawned.connect(on_player_spawned)
+
+
+func on_player_spawned():
+	var hotbar: HotbarLayout= Global.player.action_state_machine.build_state.hotbar_layout
+	var ctr:= 1
+	for block in default_build_hotbar_layout:
+		hotbar.assign(HotkeyAssignmentBuildBlock.new(ctr, block))
+		ctr+= 1
+
+func is_enabled()-> bool:
+	return true

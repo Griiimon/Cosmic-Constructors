b62b37fa65457f5691bdea6bfecfcf63ea5718f2
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-12 01:01:22 +0100

    make stone slopes work

--- game/test/custom planet test/custom_generator.gd
+++ game/test/custom planet test/custom_generator.gd
@@ -5,6 +5,9 @@ extends VoxelGeneratorScript
 
 @export var height_noise: FastNoiseLite
 @export var height_factor: float= 100.0
+
+@export var slope_stone_threshold: float= 0.8
+
 @export var mountains_noise: FastNoiseLite
 @export var mountains_threshold: float= 0.3
 @export var mountains_multiplier: float= 5.0
@@ -17,6 +20,8 @@ extends VoxelGeneratorScript
 # Change channel to SDF
 const channel : int = VoxelBuffer.CHANNEL_SDF
 
+var data: Dictionary
+
 
 
 func _get_used_channels_mask( ):
@@ -28,7 +33,9 @@ func _generate_block(out_buffer : VoxelBuffer, origin_in_voxels : Vector3i, lod 
 	var height_arr= Array2D.new(size.x, size.z)
 	var normals_arr= Array2D.new(size.x, size.z)
 	
-	if true: #lod == 0:
+	#prints(origin_in_voxels, ":", lod, size)
+	
+	if true: #not data.has(origin_in_voxels):
 		for rz in size.z:
 			for rx in size.x:
 				var pos_world := Vector3(origin_in_voxels) + Vector3(rx << lod, 0, rz << lod)
@@ -49,16 +56,20 @@ func _generate_block(out_buffer : VoxelBuffer, origin_in_voxels : Vector3i, lod 
 				var height: float= height_arr.read(rx, rz)
 				
 				var h1: float= height
+				var h2: float= height
+				var h3: float= height
 				if rx > 0:
 					h1= height_arr.readv(Vector2i(rx - 1, rz))
-				var h2: float= height
 				if rx < size.x - 1 and rz > 0:
 					h2= height_arr.readv(Vector2i(rx + 1, rz - 1))
-				var h3: float= height
 				if rx < size.x - 1 and rz < size.z - 1:
 					h3= height_arr.readv(Vector2i(rx + 1, rz + 1))
-
-				var plane:= Plane(Vector3(rx - 1, h1, rz), Vector3(rx + 1, h2, rz - 1), Vector3(rx + 1, h3, rz + 1))
+				
+				var plane:= Plane(Vector3((rx - 1) << lod, h1, rz << lod), Vector3((rx + 1) << lod, h2, (rz - 1) << lod), Vector3((rx + 1) << lod, h3, (rz + 1) << lod))
+				#var plane:= Plane(Vector3((rx - 1) * pow(2, 0), h1, rz * pow(2, lod)), Vector3((rx + 1) * pow(2, lod), h2, (rz - 1) * pow(2, lod)), Vector3((rx + 1) * pow(2, lod), h3, (rz + 1) * pow(2, lod)))
+				##var plane:= Plane(Vector3((rx - 1) , h1, rz ), Vector3((rx + 1) , h2, (rz - 1) ), Vector3((rx + 1) , h3, (rz + 1) ))
+				#if lod == 0:
+					#prints(lod, plane.normal)
 				normals_arr.put(rx, rz, plane.normal)
 				
 		
@@ -67,9 +78,10 @@ func _generate_block(out_buffer : VoxelBuffer, origin_in_voxels : Vector3i, lod 
 		for rx in size.x:
 			var pos_world := Vector3(origin_in_voxels) + Vector3(rx << lod, 0, rz << lod)
 
-			var mountains: float= mountains_noise.get_noise_2d(pos_world.x, pos_world.z)
+			#var mountains: float= mountains_noise.get_noise_2d(pos_world.x, pos_world.z)
 			
 			var height: float= height_arr.read(rx, rz)
+			var slope: float= Vector3.UP.dot(normals_arr.read(rx, rz))
 
 			for ry in out_buffer.get_size().y:
 				pos_world.y = origin_in_voxels.y + (ry << lod)
@@ -81,7 +93,7 @@ func _generate_block(out_buffer : VoxelBuffer, origin_in_voxels : Vector3i, lod 
 				out_buffer.set_voxel(VoxelTool.vec4i_to_u16_indices(Vector4i(0, 1, 2, 3)), rx, ry, rz, VoxelBuffer.CHANNEL_INDICES)
 				var weights:= Color(1, 0, 0, 0)
 #				if abs(mountains) > mountains_threshold or pos_world.y < height - 10 * (lod + 1):
-				if Vector3.UP.dot(normals_arr.read(rx, rz)) < 0.5:
+				if slope < slope_stone_threshold:# - lod * 0.05:
 					weights= Color(0, 1, 0, 0)
 				elif pos_world.y < height - 2 * (lod * 5 + 1):
 					weights= Color(0, 0, 0, 1)

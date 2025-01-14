5cabe4d1e8386c0cc9074613f299426dd0bc9524
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-14 03:03:16 +0100

    implement SuspensionEffector

--- game/core/block/instances/steering_instance.gd
+++ game/core/block/instances/steering_instance.gd
@@ -1,26 +1,5 @@
-extends BlockInstance
+extends SuspensionEffector
 
-var suspension: SuspensionInstance
-
-
-
-func on_placed(grid: BlockGrid, grid_block: GridBlock):
-	init(grid, grid_block)
-
-
-func on_neighbor_placed(grid: BlockGrid, grid_block: BaseGridBlock, neighbor_block_pos: Vector3i):
-	init(grid, grid_block, neighbor_block_pos)
-
-
-func init(grid: BlockGrid, grid_block: BaseGridBlock, neighbor_block_pos= null):
-	if suspension: 
-		breakpoint
-		return
-	
-	var neighbor_instances: Array[BlockInstance]= get_neighbor_class_instances(grid, grid_block, "SuspensionInstance", neighbor_block_pos, false)
-	if neighbor_instances.is_empty(): return
-	
-	suspension= neighbor_instances[0]
 
 
 func physics_tick(grid: BlockGrid, _grid_block: GridBlock, _delta: float):

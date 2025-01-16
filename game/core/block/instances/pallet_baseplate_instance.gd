57c02200e91e0ca53791db41a7108eb3d8b0faaf
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-16 01:24:36 +0100

    Update pallet_baseplate_instance.gd

--- game/core/block/instances/pallet_baseplate_instance.gd
+++ game/core/block/instances/pallet_baseplate_instance.gd
@@ -1,21 +1,28 @@
 extends BlockInstanceOnOff
 
-@export var alignment_factor: float= 10
+@export var alignment_factor: float= 500
+@export var pull_strength: float= 10
+@export var max_pull_velocity: float= 5
+
 
 @onready var area: Area3D = $Area3D
 
 
 
+func _ready() -> void:
+	super()
+	default_interaction_property= active
+
+
 func physics_tick(_grid: BlockGrid, _grid_block: GridBlock, _delta: float):
+	if not active.is_true(): return
+	
 	for body: ObjectEntity in area.get_overlapping_bodies():
 		assert(body)
 		if body is Pallet:
 			var pallet: Pallet= body
 			
-			
 			var vec: Vector3= area.global_position - pallet.global_position
-			#rigidbody.linear_velocity= vec.normalized() * pow(vec.length() * 2, 2)
-			pallet.linear_velocity= vec * 10  
-			#var strength: float= 10.0 / pallet.mass
-			#rigidbody.linear_velocity.limit_length(10 * strength)
-			pallet.apply_torque(Utils.calc_angular_velocity(pallet.global_basis, area.global_basis) * alignment_factor)
+			pallet.linear_velocity= vec * pull_strength 
+			pallet.linear_velocity= pallet.linear_velocity.limit_length(max_pull_velocity)
+			pallet.apply_torque(Utils.calc_angular_velocity(pallet.global_basis, area.global_basis, true) * alignment_factor)

dcea31e5744989b583dfa57439d693cb9e62b3d6
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-20 02:34:07 +0100

    add misc settings

--- game/global/game data/game_settings.gd
+++ game/global/game data/game_settings.gd
@@ -2,3 +2,4 @@ class_name GameSettings
 extends Resource
 
 @export var video_settings: VideoSettings
+@export var misc_settings: MiscSettings
---
+++ game/global/game data/misc_settings.gd
@@ -0,0 +1,5 @@
+class_name MiscSettings
+extends Resource
+
+
+@export_dir var blueprint_folder: String

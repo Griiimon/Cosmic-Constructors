39747b515995263862e4f6ff651fc49568a0c095
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-22 19:15:26 +0100

    add ControlSettings

---
+++ game/global/game data/control_settings.gd
@@ -0,0 +1,4 @@
+class_name ControlSettings
+extends Resource
+
+@export var mouse_sensitivity: float= 1.0
--- game/global/game data/game_settings.gd
+++ game/global/game data/game_settings.gd
@@ -2,4 +2,5 @@ class_name GameSettings
 extends Resource
 
 @export var video_settings: VideoSettings
+@export var control_settings: ControlSettings
 @export var misc_settings: MiscSettings

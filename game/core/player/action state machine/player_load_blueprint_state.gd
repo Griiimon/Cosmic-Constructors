4a1e51177c00acce983e9f95caa910f2e039f0b9
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-01-26 01:31:49 +0100

    implement blueprint placing

--- game/core/player/action state machine/player_load_blueprint_state.gd
+++ game/core/player/action state machine/player_load_blueprint_state.gd
@@ -2,17 +2,39 @@ class_name PlayerLoadBlueprintState
 extends PlayerActionStateMachineState
 
 var grid: BlockGrid
+var stored_collision_mask: int
 
 
 
 func _ready() -> void:
 	super()
 	SignalManager.blueprint_selected.connect(load_blueprint)
+	Global.ui.blueprint_scroll_panel.closed.connect(on_blueprint_panel_closed)
 
 
 func on_enter():
 	Global.ui.open_blueprint_menu()
 
+
+func on_exit():
+	grid= null
+
+
+func on_physics_process(_delta: float):
+	if not grid: return
+	grid.position= player.get_look_ahead_pos(10)
+
+
+func on_unhandled_input(event: InputEvent):
+	if not grid: return
+	
+	if event.is_pressed() and event.is_action("place_blueprint"):
+		grid.freeze= false
+		grid.collision_layer= CollisionLayers.GRID
+		grid.collision_mask= stored_collision_mask
+		finished.emit()
+		return
+	
 
 func load_blueprint(blueprint_name: String):
 	var blueprint_folder: String= GameData.game_settings.misc_settings.blueprint_folder
@@ -33,4 +55,11 @@ func load_blueprint(blueprint_name: String):
 
 	grid= BlockGrid.pre_deserialize(json.data, player.world, player.get_look_ahead_pos(10))
 	grid.deserialize(json.data)
-	finished.emit()
+	grid.collision_layer= 0
+	stored_collision_mask= grid.collision_mask
+	grid.collision_mask= 0
+
+
+func on_blueprint_panel_closed():
+	if is_current_state():
+		finished.emit()

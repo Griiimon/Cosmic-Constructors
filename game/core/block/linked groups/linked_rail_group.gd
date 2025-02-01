28cb70f76401bc42111e9ed36183fe4950ba5fa4
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-02-01 02:03:02 +0100

    fix global_transform

--- game/core/block/linked groups/linked_rail_group.gd
+++ game/core/block/linked groups/linked_rail_group.gd
@@ -13,14 +13,15 @@ var motor_velocity: BlockPropFloat
 
 func _init(_grid: BlockGrid, virtual: bool= false):
 	super(_grid, virtual)
-	global_transform= grid.global_transform
 
 
 func add_block(grid_block: GridBlock, value: Variant= 0):
 	super(grid_block, value)
 	
 	#if grid_block.local_pos == Vector3i.ZERO: return
-	if blocks.size() == 1: return
+	if blocks.size() == 1: 
+		global_transform= grid_block.get_block_instance().global_transform
+		return
 	
 	var dir_to_block: Vector3= global_transform.origin.direction_to(grid.get_global_block_pos(grid_block.local_pos))
 	if joint.global_basis.x.dot(dir_to_block) > 0:

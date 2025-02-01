f8991c6f777fd3faad632ce2ed9e2696c7d56bd7
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-02-01 23:20:00 +0100

    create BlueprintData class

---
+++ game/core/misc/blueprints/blueprint_data.gd
@@ -0,0 +1,85 @@
+class_name BlueprintData
+
+var grid: BlockGrid
+var sub_grids_node: Node3D
+var position: Vector3:
+	set(p):
+		position= p
+		assert(grid)
+		grid.position= position
+
+
+
+func place(pos: Vector3, world: World):
+	position= pos
+	
+	if sub_grids_node:
+		for child in sub_grids_node.get_children():
+			child.reparent(world.grids)
+	
+		sub_grids_node.queue_free()
+		sub_grids_node= null
+
+
+#func load_blueprint(blueprint_name: String, model_only: bool= false, world: World= null):
+func load_blueprint(data: Array, model_only: bool= false, world: World= null):
+	#var data: Array= get_blueprint_data_from_file(blueprint_name)
+	
+	var all_grids: Array[Dictionary]
+	all_grids.assign(data)
+
+	#var world: World= null
+	#if not model_only:
+		#world= player.world
+	
+	var sub_grids: Array[BlockGrid]
+	var sub_grid_id_remaps: Dictionary
+	
+	for grid_data in all_grids:
+		if not grid:
+			grid= BlockGrid.pre_deserialize(grid_data, world, position)
+		else:
+			var sub_grid: BlockGrid= BlockGrid.pre_deserialize(grid_data, world, position)
+			sub_grids.append(sub_grid)
+			var grid_data_id: int= grid_data["local_id"]
+			sub_grid_id_remaps[grid_data_id]= sub_grid.id
+	
+	grid.deserialize(all_grids.pop_front(), model_only, null, sub_grids, sub_grid_id_remaps)
+
+#	if model_only:
+	if not all_grids.is_empty():
+		sub_grids_node= Node3D.new()
+		sub_grids_node.name= "Sub Grids"
+		grid.add_child(sub_grids_node)
+		
+	for i in all_grids.size():
+		var sub_grid: BlockGrid= sub_grids[i]
+		sub_grid.deserialize(all_grids[i], model_only, grid, sub_grids, sub_grid_id_remaps)
+		if model_only:
+			grid.add_child(sub_grid)
+		else:
+			sub_grid.reparent(grid, false)
+
+
+func clear():
+	grid.queue_free()
+	grid= null
+
+
+static func get_blueprint_data_from_file(file_name: String)-> Array:
+	var blueprint_folder: String= GameData.game_settings.misc_settings.blueprint_folder
+	var base_directory:= DirAccess.open(blueprint_folder)
+	if not base_directory:
+		push_error("Cannot open directory ", blueprint_folder)
+
+	if not base_directory.dir_exists(file_name):
+		push_error("Cannot find blueprint directory ", blueprint_folder + "/" + file_name)
+
+	base_directory.change_dir(file_name)
+
+	var file:= FileAccess.open(base_directory.get_current_dir() + "/" + "blueprint.json", FileAccess.READ)
+	var json= JSON.new()
+	if json.parse(file.get_as_text()) != OK:
+		push_error("Error parsing blueprint ", json.get_error_message())
+	file.close()
+	return json.data
--- game/core/player/action state machine/player_load_blueprint_state.gd
+++ game/core/player/action state machine/player_load_blueprint_state.gd
@@ -1,9 +1,8 @@
 class_name PlayerLoadBlueprintState
 extends PlayerActionStateMachineState
 
-var grid: BlockGrid
 var current_blueprint: String
-var sub_grids_node: Node3D
+var blueprint_data: BlueprintData
 
 
 
@@ -18,93 +17,33 @@ func on_enter():
 
 
 func on_exit():
-	grid= null
+	blueprint_data= null
 	current_blueprint= ""
 
 
 func on_physics_process(_delta: float):
-	if not grid: return
-	grid.position= player.get_look_ahead_pos(10)
+	if not blueprint_data: return
+	blueprint_data.position= player.get_look_ahead_pos(10)
 
 
 func on_unhandled_input(event: InputEvent):
-	if not grid: return
+	if not blueprint_data: return
 	
 	if event.is_pressed() and event.is_action("place_blueprint"):
-		grid.queue_free()
-		grid= null
+		blueprint_data.clear()
 		
-		load_blueprint(current_blueprint)
-		grid.position= player.get_look_ahead_pos(10)
-
-		if sub_grids_node:
-			for child in sub_grids_node.get_children():
-				child.reparent(player.world.grids)
+		blueprint_data.load_blueprint(BlueprintData.get_blueprint_data_from_file(current_blueprint), false, player.world)
+		blueprint_data.place(player.get_look_ahead_pos(10), player.world)
 		
-			sub_grids_node.queue_free()
-			sub_grids_node= null
-			
 		finished.emit()
 		return
-	
-
-func load_blueprint(blueprint_name: String, model_only: bool= false):
-	var blueprint_folder: String= GameData.game_settings.misc_settings.blueprint_folder
-	var base_directory:= DirAccess.open(blueprint_folder)
-	if not base_directory:
-		push_error("Cannot open directory ", blueprint_folder)
-
-	if not base_directory.dir_exists(blueprint_name):
-		push_error("Cannot find blueprint directory ", blueprint_folder + "/" + blueprint_name)
-
-	base_directory.change_dir(blueprint_name)
-
-	var file:= FileAccess.open(base_directory.get_current_dir() + "/" + "blueprint.json", FileAccess.READ)
-	var json= JSON.new()
-	if json.parse(file.get_as_text()) != OK:
-		push_error("Error parsing blueprint ", json.get_error_message())
-	file.close()
-
-	var all_grids: Array[Dictionary]
-	all_grids.assign(json.data)
-
-	var world: World= null
-	if not model_only:
-		world= player.world
-	
-	var sub_grids: Array[BlockGrid]
-	var sub_grid_id_remaps: Dictionary
-	
-	for grid_data in all_grids:
-		if not grid:
-			grid= BlockGrid.pre_deserialize(grid_data, world, player.get_look_ahead_pos(10))
-		else:
-			var sub_grid: BlockGrid= BlockGrid.pre_deserialize(grid_data, world, player.get_look_ahead_pos(10))
-			sub_grids.append(sub_grid)
-			var grid_data_id: int= grid_data["local_id"]
-			sub_grid_id_remaps[grid_data_id]= sub_grid.id
-	
-	grid.deserialize(all_grids.pop_front(), model_only, null, sub_grids, sub_grid_id_remaps)
-
-#	if model_only:
-	if not all_grids.is_empty():
-		sub_grids_node= Node3D.new()
-		sub_grids_node.name= "Sub Grids"
-		grid.add_child(sub_grids_node)
-		
-	for i in all_grids.size():
-		var sub_grid: BlockGrid= sub_grids[i]
-		sub_grid.deserialize(all_grids[i], model_only, grid)
-		if model_only:
-			grid.add_child(sub_grid)
-		else:
-			sub_grid.reparent(grid, false)
 
 
 func on_load_blueprint(blueprint_name: String):
 	current_blueprint= blueprint_name
-	load_blueprint(blueprint_name, true)
-	add_child(grid)
+	blueprint_data= BlueprintData.new()
+	blueprint_data.load_blueprint(BlueprintData.get_blueprint_data_from_file(blueprint_name), true)
+	add_child(blueprint_data.grid)
 
 
 func on_blueprint_panel_closed():

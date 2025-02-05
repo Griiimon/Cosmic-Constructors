1320ead2aee64c27bc2efcb58cdce1422690fc5f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-02-05 10:57:10 +0100

    limit_torque()

--- game/core/block/linked groups/linked_drive_shaft_group.gd
+++ game/core/block/linked groups/linked_drive_shaft_group.gd
@@ -5,18 +5,19 @@ extends LinkedBlockGroup
 
 var torque: float
 var new_torque: float
+var new_torque_limit: float= -1
 var suspensions: Array[SuspensionInstance]
 
 
 
 func tick(delta: float):
+	if new_torque_limit >= 0:
+		torque= new_torque_limit
+	new_torque_limit= -1
 	torque= max(torque, new_torque)
 	new_torque= 0
 	
-	torque*= (1 - friction * delta)
-
-	#for suspension in suspensions:
-		#suspension.input_torque= torque
+	#torque*= (1 - friction * delta)
 
 
 func add_suspension(suspension: SuspensionInstance):
@@ -31,3 +32,11 @@ func remove_suspension(suspension: SuspensionInstance):
 
 func apply_torque(input_torque: float):
 	new_torque+= input_torque
+
+
+func limit_torque(limit: float):
+	limit= abs(limit)
+	if new_torque_limit < 0:
+		new_torque_limit= limit
+	else:
+		new_torque_limit= min(new_torque_limit, limit)

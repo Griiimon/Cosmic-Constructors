bdae10d0d0daae138a95e659d4b2b7c50a0bbcb7
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-02-07 09:43:07 +0100

    Update object_entity.gd

--- game/core/object entity/object_entity.gd
+++ game/core/object entity/object_entity.gd
@@ -6,3 +6,7 @@ extends RigidBody3D
 func _ready() -> void:
 	collision_layer= CollisionLayers.PHYSICAL_OBJECTS
 	collision_mask= CollisionLayers.PLAYER + CollisionLayers.GRID + CollisionLayers.TERRAIN
+
+
+func get_world()-> World:
+	return get_parent().get_parent()

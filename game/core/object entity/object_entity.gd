1d8e786aa08232b08ec85fdfcc7ae50d9dc0f69c
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-02-07 09:59:45 +0100

    Update object_entity.gd

--- game/core/object entity/object_entity.gd
+++ game/core/object entity/object_entity.gd
@@ -8,5 +8,10 @@ func _ready() -> void:
 	collision_mask= CollisionLayers.PLAYER + CollisionLayers.GRID + CollisionLayers.TERRAIN
 
 
+func destroy():
+	# TODO network sync
+	queue_free()
+
+
 func get_world()-> World:
 	return get_parent().get_parent()

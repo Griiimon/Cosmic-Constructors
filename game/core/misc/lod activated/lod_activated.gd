3ec8a86b3c3feb4c5a29d2d90606d9d0118e336e
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-04-05 00:11:27 +0200

    add delay

--- game/core/misc/lod activated/lod_activated.gd
+++ game/core/misc/lod activated/lod_activated.gd
@@ -1,10 +1,13 @@
 class_name LodActivated
 extends Area3D
 
+const DELAY= 1.0
+
 signal terrain_initialized
 
 
 
 func activate():
+	await get_tree().create_timer(DELAY).timeout
 	terrain_initialized.emit()
 	queue_free()

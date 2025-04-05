692efe2561be5e1f4d592ebee34a7655d40b7e22
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-04-05 06:01:44 +0200

    add rotating functionality

--- game/core/block/instances/outboard_engine_instance.gd
+++ game/core/block/instances/outboard_engine_instance.gd
@@ -5,7 +5,9 @@ extends BlockInstanceOnOff
 @export var reverse_force_factor: float= 0.1
 
 @onready var reverse:= BlockPropBool.new("Reverse", false)
+@onready var engine_rotation: BlockPropFloat= BlockPropFloat.new("Rotation", 0.0, change_rotation).set_range(-40, 40).set_step_size(1)
 
+@onready var model: Node3D = $Model
 @onready var propeller: Node3D = %Propeller
 
 
@@ -20,9 +22,14 @@ func physics_tick(grid: BlockGrid, grid_block: GridBlock, _delta: float):
 	if active.is_true():
 		if grid.world.is_point_under_water(propeller.global_position):
 			var final_force: Vector3
+			var dir: Vector3= model.global_basis.z
 			if reverse.is_true():
-				final_force= force * reverse_force_factor * -global_basis.z
+				final_force= force * reverse_force_factor * -dir
 			else:
-				final_force= force * global_basis.z
+				final_force= force * dir
 
 			grid.apply_force(final_force, grid.get_block_force_offset(grid_block))
+
+
+func change_rotation():
+	model.rotation_degrees.y= engine_rotation.get_value_f()

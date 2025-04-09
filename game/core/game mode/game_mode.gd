4b077ec2e6154e2f66dc25115ee55deee1087215
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-04-09 20:11:35 +0200

    game mode large block library

--- game/core/game mode/game_mode.gd
+++ game/core/game mode/game_mode.gd
@@ -2,4 +2,5 @@ class_name GameMode
 extends Resource
 
 @export var game_scene: PackedScene
-@export var block_library: BlockLibrary
+@export var large_block_library: BlockLibrary
+@export var small_block_library: BlockLibrary

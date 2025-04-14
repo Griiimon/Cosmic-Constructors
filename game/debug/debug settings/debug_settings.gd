5ccac6bef5c25cfd2f83b12191a5d6207fd4b1a7
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-04-14 08:03:11 +0200

    Update debug_settings.gd

--- game/debug/debug settings/debug_settings.gd
+++ game/debug/debug settings/debug_settings.gd
@@ -13,8 +13,8 @@ func _ready() -> void:
 	SignalManager.player_spawned.connect(on_player_spawned)
 
 
-func on_player_spawned():
-	var hotbar: HotbarLayout= Global.player.action_state_machine.build_state.hotbar_layout
+func on_player_spawned(player: Player):
+	var hotbar: HotbarLayout= player.action_state_machine.build_state.hotbar_layout
 	var ctr:= 1
 	for block in active.default_build_hotbar_layout:
 		hotbar.assign(HotkeyAssignmentBuildBlock.new(ctr, block))
